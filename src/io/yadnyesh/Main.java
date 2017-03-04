ade5f5ff65f57770ce616e626b1e3ffa8d6d6762
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-04 19:19:23 +0530

    First Commit

---
+++ src/io/yadnyesh/Main.java
@@ -0,0 +1,25 @@
+package io.yadnyesh;
+
+/**
+ * Created by z063407 on 3/4/17.
+ */
+public class Main {
+    public static void main (String args[]){
+       int switchValue = 1;
+
+       switch(switchValue){
+           case 1:
+               System.out.println("Value was 1");
+               break;
+           case 2:
+               System.out.println("Value was 2");
+               break;
+            default:
+                System.out.println();
+                break;
+       }
+
+    }
+
+
+}
