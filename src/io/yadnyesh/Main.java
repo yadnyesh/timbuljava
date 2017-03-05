9ca033c245ad073dd99e3417e95e011f5f847543
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-05 18:12:56 +0530

    05th Mar

--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -9,11 +9,16 @@ public class Main {
         System.out.println(calculateInterest(10000, 3.0));
         System.out.println(calculateInterest(10000, 4));
         System.out.println(calculateInterest(10000.0, 5.0));
-       }
 
-    for(int i=0; i<5, ) {
-        System.out.println("Loop " + i + " hello");
+        for(int i=0; i<5; i++) {
+            System.out.println("Loop " + i + " hello");
+        }
+
+        for (int i=8; i > 1; i--){
+            System.out.println(String.format("%.2f", calculateInterest(10000.0, i)));
+        }
     }
+
 
     public static double calculateInterest(double amount, double interestRate){
         return (amount*(interestRate/100));
