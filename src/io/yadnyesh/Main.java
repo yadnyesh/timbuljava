68c7934a6e3b115b300e0dfde0f5f9ea7876e081
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-06 18:15:53 +0530

    Freestyle

--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -14,7 +14,7 @@ public class Main {
             System.out.println("Loop " + i + " hello");
         }
 
-        for (int i=8; i > 1; i--){
+        for (int i=9; i > 1; i--){
             System.out.println(String.format("%.2f", calculateInterest(10000.0, i)));
         }
     }
