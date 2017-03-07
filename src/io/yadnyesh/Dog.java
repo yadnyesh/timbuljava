9a1719f085988df8754c99af6cd1a36a87daddb1
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-07 17:58:29 +0530

    number 40

--- src/io/yadnyesh/Dog.java
+++ src/io/yadnyesh/Dog.java
@@ -4,5 +4,14 @@ package io.yadnyesh;
  * Created by z063407 on 3/7/17.
  */
 public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
     
+    public Dog(String name, int brain, int body, int size, int weight) {
+        super(name, brain, body, size, weight);
+    }
 }
