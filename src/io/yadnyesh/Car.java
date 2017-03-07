574c7c147479713a7b4bcf374d1b36c86d738c8d
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-07 06:57:12 +0530

    Added Car Class

--- src/io/yadnyesh/Car.java
+++ src/io/yadnyesh/Car.java
@@ -4,4 +4,13 @@ package io.yadnyesh;
  * Created by z063407 on 3/7/17.
  */
 public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    
+
 }
