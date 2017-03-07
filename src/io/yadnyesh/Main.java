90d94169b709946d5532b76b13e8ef8106b46e7a
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-07 18:21:28 +0530

    number 41

--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -9,8 +9,6 @@ public class Main {
         Animal animal = new Animal("Animal", 1, 1, 5,5);
 
         Dog dog = new Dog("Yorkie", 8, 29, 2, 4, 1,20, "long silky");
-        dog.eat();
-        dog.walk();
         dog.run();
     }
 }
