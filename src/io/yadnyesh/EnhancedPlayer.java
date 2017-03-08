ddeeba8550f77e91d75a2e462b0e46b59294a7a3
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-08 07:54:07 +0530

    Lecture 48

--- src/io/yadnyesh/EnhancedPlayer.java
+++ src/io/yadnyesh/EnhancedPlayer.java
@@ -5,29 +5,29 @@ package io.yadnyesh;
  */
 public class EnhancedPlayer {
     private String name;
-    private int health = 100;
+    private int hitPoints = 100;
     private String weapon;
 
     public EnhancedPlayer(String name, int health, String weapon) {
         this.name = name;
         if(health > 0 && health <= 100) {
-            this.health = health;
+            this.hitPoints = health;
         }
         this.weapon = weapon;
     }
 
     public void loseHealth(int damage) {
-        this.health = this.health - damage;
-        if(this.health <= 0) {
+        this.hitPoints = this.hitPoints - damage;
+        if(this.hitPoints <= 0) {
             System.out.println("The Player knocked out");
         }
     }
 
     public int healthRemaining() {
-        return health;
+        return hitPoints;
     }
 
     public int getHealth() {
-        return health;
+        return hitPoints;
     }
 }
