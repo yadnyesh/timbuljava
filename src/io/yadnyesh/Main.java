b6b8354940b7d3006fcccce86a53ab810fcc037a
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-08 13:10:04 +0530

    After Lecture 53

--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -6,5 +6,24 @@ package io.yadnyesh;
 public class Main {
     public static void main (String args[]) {
 
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.12);
+
+        price = hamburger.itemizeHamburger();
+        System.out.println("Total Burger Price is: $" + price);
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        healthyBurger.itemizeHamburger();
+        System.out.println("Total Healthy Burger Price is: $" + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+        deluxeBurger.itemizeHamburger();
+
     }
 }
