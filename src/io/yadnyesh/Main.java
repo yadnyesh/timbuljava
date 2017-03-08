b303112a1fc83c04e180e2d5a1f8d948f0df165f
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-08 11:44:13 +0530

    Before the exercise

--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -5,22 +5,6 @@ package io.yadnyesh;
  */
 public class Main {
     public static void main (String args[]) {
-//        Player player = new Player();
-//        player.name = "Tim";
-//        player.health = 20;
-//        player.weapon = "sword";
-//
-//        int damage = 10;
-//        player.loseHealth(damage);
-//        System.out.println("Remaining Health: " + player.healthRemaining());
-//
-//        damage = 11;
-//        player.loseHealth(damage);
-//        System.out.println("Remaining Health: " + player.healthRemaining());
-
-
-        EnhancedPlayer player = new EnhancedPlayer("tim", 200, "sword");
-        System.out.println("Initial health is " + player.getHealth());
 
     }
 }
