7e1c5160982a5895a7a9d36ac4e4bd868f8b1c70
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-09 19:52:53 +0530

    Multi Threading

--- src/io/threads/App.java
+++ src/io/threads/App.java
@@ -1,5 +1,7 @@
 package io.threads;
 
+import java.util.Random;
+
 /**
  * Created by z063407 on 3/9/17.
  */
@@ -7,49 +9,8 @@ package io.threads;
 
 public class App {
 
-    private int count = 0;
-
-    public synchronized void increment() {
-        count++;
-    }
-
-    public static void main(String[] args) {
-        App app = new App();
-        app.doWork();
-
-    }
-
-    public void doWork() {
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10000; i++) {
-                    increment();
-                }
-            }
-        });
-
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10000; i++) {
-                    increment();
-                }
-            }
-        });
-
-        t1.start();
-        t2.start();
-
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
-        System.out.println("Count is: " + count);
+    public static void main(String[] args){
+        System.out.println(new Random().nextInt(100));
     }
 
 }
--- src/io/threads/Worker.java
+++ src/io/threads/Worker.java
@@ -1,11 +1,24 @@
 package io.threads;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by z063407 on 3/9/17.
  */
 public class Worker {
 
+    private Random random = new Random();
+
+    private List<Integer> list1 = new ArrayList<Integer>();
+    private List<Integer> list2 = new ArrayList<Integer>();
+
+
+
+
+
     public void main() {
-        System.out.println("Hello");
+        System.out.println(new Random().nextInt(100));
     }
 }
