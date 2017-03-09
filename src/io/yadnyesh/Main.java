0afda2dcde23bdcd3679a33ed2f1b90953925e79
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-09 10:42:54 +0530

    Lecture 56 complete

--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -1,66 +1,18 @@
 package io.yadnyesh;
 
+import java.util.Scanner;
+
 /**
  * Created by z063407 on 3/4/17.
  */
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
 
-    public static void main (String args[]) {
-        int[] myIntegers = getIntegers(5);
-        int[] sorted = sortIntegers(myIntegers);
-        printArray(sorted);
+    public static void main(String[] args) {
 
-//        for(int i = 0; i < myIntegers.length; i++) {
-//            System.out.println("Element " + i + "is : " + myIntegers[i]);
-//        }
     }
-
-    public static int[] getIntegers(int capacity) {
-        System.out.println("Enter " + capacity + " of integer values.");
-        int[] array = new int[capacity];
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = scanner.nextInt();
-        }
-        return array;
-    }
-
-    public static void printArray(int array[]){
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("Element " + i + "is : " + array[i]);
-        }
-    }
-
-    public static int[] sortIntegers(int[] array) {
-//        int[] sortedArray = new int[array.length];
-//        for(int i=0; i < array.length; i++) {
-//            sortedArray[i] = array[i];
-//        }
-
-        int[] sortedArray = Arrays.copyOf(array, array.length);
-        boolean flag = true;
-        int temp;
-        while(flag){
-            flag = false;
-            for(int i=0; i < sortedArray.length-1; i++){
-                if(sortedArray[i] < sortedArray[i+1]) {
-                    temp = sortedArray[i];
-                    sortedArray[i] = sortedArray[i+1];
-                    sortedArray[i+1] = temp;
-                    flag = true;
-                }
-            }
-        }
-
-
-        return sortedArray;
-    }
-
 }
 
