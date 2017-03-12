166b44875e6b9d2d600b6542339e34ee51b76281
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-12 12:04:49 +0530

    Lecture 63

--- src/io/yadnyesh/Branch.java
+++ src/io/yadnyesh/Branch.java
@@ -29,7 +29,19 @@ public class Branch {
 
         if(existingCustomer != null) {
             existingCustomer.addTransaction(amount);
+            return true;
         }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName) {
+        for (int i = 0; i < customers.size(); i++) {
+            Customer checkCustomer = this.customers.get(i);
+            if(checkCustomer.getName().equals(customerName)) {
+                return checkCustomer;
+            }
+        }
+        return null;
     }
 
     public String getName() {
