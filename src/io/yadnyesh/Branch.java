6667ec02e963363994efd7c7ac31c8056555f8be
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-12 11:32:03 +0530

    Lecture 63

---
+++ src/io/yadnyesh/Branch.java
@@ -0,0 +1,50 @@
+package io.yadnyesh;
+
+import java.util.ArrayList;
+
+/**
+ * Created by z063407 on 3/12/17.
+ */
+public class Branch {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<>();
+    }
+
+    public boolean newCustomer(String name, double initialAmount) {
+        if(findCustomer(name) == null) {
+            this.customers.add(new Customer(name, initialAmount));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer existingCustomer = findCustomer(customerName);
+
+        if(existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+}
--- src/io/yadnyesh/Customer.java
+++ src/io/yadnyesh/Customer.java
@@ -1,4 +1,36 @@
 package io.yadnyesh;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
+
 public class Customer {
+    private String name;
+    private AbstractList<Double> transactions;
+
+    public Customer(String name, double initialAmount) {
+        this.name = name;
+        this.transactions = new ArrayList<>();
+        addTransaction(initialAmount);
+    }
+
+    public void addTransaction(double amount) {
+        this.transactions.add(amount);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AbstractList<Double> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(AbstractList<Double> transactions) {
+        this.transactions = transactions;
+    }
 }
