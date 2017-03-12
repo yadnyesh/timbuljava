f4b0195a1d97c4cc150521492a3c7a314158a36a
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-12 08:41:06 +0530

    12th March 2017

--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -1,6 +1,15 @@
 package io.yadnyesh;
 
+import java.util.ArrayList;
+
 public class Main {
-    
+
+    public static void main (String args[]) {
+        String[] strArray = new String[10];
+        int[] intArray = new int[10];
+
+        ArrayList<String> stringArrayList = new ArrayList<String>();
+
+    }
 }
 
