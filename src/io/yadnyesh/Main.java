622fd780a84c68b507e6ff86d9d6e0357542ca53
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-12 09:39:06 +0530

    12th March 2017

--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -2,6 +2,23 @@ package io.yadnyesh;
 
 import java.util.ArrayList;
 
+
+class IntClass {
+    private int myValue;
+
+    public IntClass(int myValue) {
+        this.myValue = myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
+
 public class Main {
 
     public static void main (String args[]) {
@@ -9,6 +26,10 @@ public class Main {
         int[] intArray = new int[10];
 
         ArrayList<String> stringArrayList = new ArrayList<String>();
+        stringArrayList.add("Tim");
+
+        ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
+        intClassArrayList.add(new IntClass(54));
 
     }
 }
