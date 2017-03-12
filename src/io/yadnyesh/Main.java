81e0b453146b61cb7df79509516826878f160ba6
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-12 08:38:33 +0530

    12th March 2017

--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -1,18 +1,6 @@
 package io.yadnyesh;
 
-import java.util.Scanner;
-
-/**
- * Created by z063407 on 3/4/17.
- */
-
 public class Main {
-
-    private static Scanner scanner = new Scanner(System.in);
-    private static GroceryList groceryList = new GroceryList();
-
-    public static void main(String[] args) {
-
-    }
+    
 }
 
