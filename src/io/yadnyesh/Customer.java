78c2499307bd2aacadc731d215ad5cb6a4c2af88
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-12 10:14:48 +0530

    Lecture 62

---
+++ src/io/yadnyesh/Customer.java
@@ -0,0 +1,7 @@
+package io.yadnyesh;
+
+/**
+ * Created by z063407 on 3/12/17.
+ */
+public class Customer {
+}
--- src/io/yadnyesh/Main.java
+++ src/io/yadnyesh/Main.java
@@ -1,58 +1,8 @@
 package io.yadnyesh;
 
-import java.util.ArrayList;
-
-
-class IntClass {
-    private int myValue;
-
-    public IntClass(int myValue) {
-        this.myValue = myValue;
-    }
-
-    public int getMyValue() {
-        return myValue;
-    }
-
-    public void setMyValue(int myValue) {
-        this.myValue = myValue;
-    }
-}
-
 public class Main {
 
     public static void main (String args[]) {
-        String[] strArray = new String[10];
-        int[] intArray = new int[10];
-
-        ArrayList<String> stringArrayList = new ArrayList<String>();
-        stringArrayList.add("Tim");
-
-        ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
-        intClassArrayList.add(new IntClass(54));
-
-        Integer integer = new Integer(54);
-        Double doubleValue = new Double(12.25);
-
-        ArrayList<Integer> intArrayList = new ArrayList<>();
-
-        for (int i = 0; i <= 10; i++) {
-            //intArrayList.add(Integer.valueOf(i));
-            intArrayList.add(i);
-        }
-
-        for (int i = 0; i <= 10; i++) {
-            //System.out.println(i + " --> " +  intArrayList.get(i)) ;
-        }
-
-        ArrayList<Double> myDoubleValues = new ArrayList<>();
-        for (double dbl=0.0;  dbl <=10; dbl += 0.5) {
-            myDoubleValues.add(dbl);
-        }
-
-        for (int i = 0; i < myDoubleValues.size(); i++) {
-            System.out.println(i + " --> " + myDoubleValues.get(i));
-        }
 
 
     }
