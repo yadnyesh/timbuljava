b7f5d6c417cb0cd27af0c3bbbfd9846a4064b9ba
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-18 14:17:42 +0530

    New File Lecture 67 & 68

--- src/io/yadnyesh/Demo.java
+++ src/io/yadnyesh/Demo.java
@@ -1,10 +1,69 @@
 package io.yadnyesh;
 
-/**
- * Created by z063407 on 3/18/17.
- */
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Demo {
     public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<>();
+        addInOrder(placesToVisit, "Sydney");
+        addInOrder(placesToVisit, "Melbourne");
+        addInOrder(placesToVisit, "Brisbane");
+        addInOrder(placesToVisit, "Perth");
+        addInOrder(placesToVisit, "Canberra");
+        addInOrder(placesToVisit, "Adelaide");
 
+        addInOrder(placesToVisit, "Adelaide");
+        addInOrder(placesToVisit, "Darwin");
+
+//        placesToVisit.add("Sydney");
+//        placesToVisit.add("Melbourne");
+//        placesToVisit.add("Brisbane");
+//        placesToVisit.add("Perth");
+//        placesToVisit.add("Canberra");
+//        placesToVisit.add("Adelaide");
+//        placesToVisit.add("Darwin");
+
+        printList(placesToVisit);
+
+//        placesToVisit.add(1, "Alice Springs");
+//
+//        printList(placesToVisit);
+//
+//        placesToVisit.remove(4);
+//
+//        printList(placesToVisit);
+    }
+
+    private static void printList(LinkedList<String> linkedList) {
+        Iterator<String> i = linkedList.iterator();
+        while (i.hasNext()){
+            System.out.println("Now Visiting " + i.next());
+        }
+        System.out.println("==================================END=================================");
+    }
+
+    private static boolean addInOrder(LinkedList linkedList, String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+
+        while(stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
+
+            if(comparison ==0){
+                System.out.println(newCity + " already included as destination");
+                return false;
+            } else if (comparison > 0) {
+                //newCity to appear before the city being matched
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if (comparison < 0) {
+                stringListIterator.add(newCity);
+                return true;
+            }
+        }
+        stringListIterator.add(newCity);
+        return true;
     }
 }
