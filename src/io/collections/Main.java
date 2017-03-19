52a057e28d93895d90dec35a647bc372386adce4
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-19 18:30:19 +0530

    Binary Search - 102

--- src/io/collections/Main.java
+++ src/io/collections/Main.java
@@ -9,17 +9,17 @@ public class Main {
         Theatre theatre = new Theatre("Olympian", 8 , 12);
         //theatre.getSeats();
 
-        if(theatre.reserveSeat("H09")) {
+        if(theatre.reserveSeat("D12")) {
             System.out.println("Please pay...");
         } else {
             System.out.println("Sorry, seat is taken..");
         }
 
-        if(theatre.reserveSeat("H11")) {
-            System.out.println("Please pay...");
-        } else {
-            System.out.println("Sorry, seat is taken..");
-        }
+//        if(theatre.reserveSeat("H11")) {
+//            System.out.println("Please pay...");
+//        } else {
+//            System.out.println("Sorry, seat is taken..");
+//        }
     }
 }
 
--- src/io/collections/Theatre.java
+++ src/io/collections/Theatre.java
@@ -29,18 +29,21 @@ public class Theatre {
     public boolean reserveSeat(String seatNumber) {
         int low = 0;
         int high = seats.size()-1;
-        int mid = (low + high) / 2;
-        Seat midVal = seats.get(mid);
-        int cmp = midVal.getSeatNumber().compareTo(seatNumber);
 
-        if (cmp < 0) {
-            low = mid +1;
-        } else if (cmp > 0) {
-            high = mid -1;
-        } else {
-            return seats.get(mid).reserve();
+        while(low <= high) {
+            System.out.print(".");
+            int mid = (low + high) / 2;
+            Seat midVal = seats.get(mid);
+            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+
+            if (cmp < 0) {
+                low = mid +1;
+            } else if (cmp > 0) {
+                high = mid -1;
+            } else {
+                return seats.get(mid).reserve();
+            }
         }
-
         System.out.println("There is no seat " + seatNumber);
         return false;
     }
