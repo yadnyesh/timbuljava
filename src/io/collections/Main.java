c22524ccc8ca7642156709feec2372a6aba3264c
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-03-29 17:26:11 +0530

    Merge Sort - 103

--- src/io/collections/Main.java
+++ src/io/collections/Main.java
@@ -1,7 +1,5 @@
 package io.collections;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -11,43 +9,22 @@ public class Main {
 
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Olympian", 8 , 12);
-        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
-        printList(seatCopy);
 
-        seatCopy.get(1).reserve();
-        if(theatre.reserveSeat("A02")){
-            System.out.println("Please pay for A02");
+        if(theatre.reserveSeat("D12")) {
+            System.out.println("Please pay for D12");
         } else {
             System.out.println("Seat already reserved");
         }
 
-        Collections.shuffle(seatCopy);
-        System.out.println("Printing seatCopy");
-        printList(seatCopy);
-        System.out.println("Printing theatre.seat");
-        printList(theatre.seats);
-        sortList(seatCopy);
-        System.out.println("Printing sorted list");
-        printList(seatCopy);
 
     }
 
     public static void printList(List<Theatre.Seat> list) {
         for(Theatre.Seat seat : list) {
-            System.out.print(" " + seat.getSeatNumber());
+            System.out.print(" " + seat.getSeatNumber() + " " + seat.getSeatNumber());
         }
         System.out.println();
         System.out.println("=========================================================================");
-    }
-
-    public static void sortList(List<? extends Theatre.Seat> list) {
-        for (int i=0; i < list.size()-1; i++){
-            for(int j = i+1; j < list.size(); j++) {
-                if(list.get(i).compareTo(list.get(j)) > 0 ){
-                    Collections.swap(list, i,j);
-                }
-            }
-        }
     }
 }
 
