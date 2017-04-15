2c169cb6c702f4cc02380fece6bdf1d42b4c9466
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-04-15 11:25:03 +0530

    Merge Sort - 105

--- src/io/collections/MapProgram.java
+++ src/io/collections/MapProgram.java
@@ -17,5 +17,11 @@ public class MapProgram {
 
         System.out.println(languages.containsKey("Java"));
         System.out.println(languages.get("Java"));
+
+        System.out.println("===================");
+
+        for (String key: languages.keySet()) {
+            System.out.println(key + " : " + languages.get(key));
+        }
     }
 }
