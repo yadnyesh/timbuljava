4bf967a9af213f75089367d0da236849cca7e317
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-04-20 17:15:45 +0530

    Merge Sort - 105

---
+++ src/io/lamdas/Greeter.java
@@ -0,0 +1,17 @@
+package io.lamdas;
+
+/**
+ * Created by z063407 on 4/20/17.
+ */
+public class Greeter {
+
+    public void greet() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        Greeter g = new Greeter();
+        g.greet();
+
+    }
+}
