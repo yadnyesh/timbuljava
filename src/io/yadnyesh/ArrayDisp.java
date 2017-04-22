6354e226fbf2c270db9e3d8c5c429b48949fb204
Yadnyesh Bharat Juvekar <yadnyesh.bharatjuvekar@target.com>
2017-04-23 00:01:46 +0530

    Generic

--- src/io/yadnyesh/ArrayDisp.java
+++ src/io/yadnyesh/ArrayDisp.java
@@ -26,6 +26,9 @@ public class ArrayDisp {
         names[8] = "Brian";
         names[9] = "Greg";
         Display(names);
+        Integer[] numbers = new Integer[] {1,2,3,4,5,6,7,8,9,0};
+        System.out.println("");
+        Display(numbers);
     }
 
 
