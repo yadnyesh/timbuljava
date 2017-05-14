8929c501d8468baf7626f10fe203b845622e9eca
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-05-14 19:22:42 +0530

    Test

--- src/io/collections/StockItem.java
+++ src/io/collections/StockItem.java
@@ -3,6 +3,8 @@ package io.collections;
 /**
  * Created by z063407 on 5/14/17.
  */
-public class StockItem {
-    
+public class StockItem implements Comparable<StrockItem>{
+    private final String name;
+    private double price;
+    private int quantityStock = 0; /// can also be initialized later
 }
