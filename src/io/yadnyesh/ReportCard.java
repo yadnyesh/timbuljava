5f813e711734b9699c8fb7a25b6668914c8002d7
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-05-15 18:49:20 +0530

    Changed the StudentMain Class

--- src/io/yadnyesh/ReportCard.java
+++ src/io/yadnyesh/ReportCard.java
@@ -4,4 +4,21 @@ package io.yadnyesh;
  * Created by z063407 on 5/15/17.
  */
 public class ReportCard {
+    private int mathScore;
+
+    public ReportCard(int mathScore) {
+        this.mathScore = mathScore;
+    }
+
+    public ReportCard() {
+        this.mathScore = (int) (Math.random()*101) + 1;
+    }
+
+    public int getMathScore() {
+        return mathScore;
+    }
+
+    public void setMathScore(int mathScore) {
+        this.mathScore = mathScore;
+    }
 }
