e67f3eca001483ad4aec16ac54a43939c2579e87
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-05-15 18:40:32 +0530

    Created the Student Class

---
+++ src/io/yadnyesh/Student.java
@@ -0,0 +1,21 @@
+package io.yadnyesh;
+
+/**
+ * Created by z063407 on 5/15/17.
+ */
+public class Student {
+
+    private String registrationNumber;
+
+    public Student(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+}
