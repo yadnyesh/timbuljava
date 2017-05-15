5a4c86d342478fb4f4f5d0410032c1bbc8a4f81f
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-05-15 18:45:54 +0530

    Changed the StudentMain Class

---
+++ src/io/yadnyesh/ReportCard.java
@@ -0,0 +1,7 @@
+package io.yadnyesh;
+
+/**
+ * Created by z063407 on 5/15/17.
+ */
+public class ReportCard {
+}
--- src/io/yadnyesh/StudentMain.java
+++ src/io/yadnyesh/StudentMain.java
@@ -1,5 +1,7 @@
 package io.yadnyesh;
 
+import java.util.HashMap;
+
 /**
  * Created by z063407 on 5/15/17.
  */
@@ -7,7 +9,10 @@ public class StudentMain {
     public static void main(String[] args) {
         Student s = new Student("H234");
         Student s2 = new Student("H234");
-        
+
+        System.out.println(s.equals(s2));
+
+        Map <Student, ReportCard> studentsReport = new HashMap<Student, ReportCard>();
     }
 
 }
