c8f646224b6d542ceb76bf047e58fc1c06037c55
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-07-09 22:11:02 +0530

    Section 2 Lecture 8 - Multithreading - Executors - Lecture 12

--- src/io/common/LoopTaskA.java
+++ src/io/common/LoopTaskA.java
@@ -10,14 +10,18 @@ public class LoopTaskA implements Runnable {
     private int id;
 
         public void run() {
+            System.out.println(" #####  <TASK-" + id + " > STARTING ########");
+
             for (int i = 10; i > 0; i--) {
-                System.out.println("< " + id + " >" + "Tick Tick " + i);
+                System.out.println("< Task " + id + " >" + "Tick Tick " + i);
                 try {
-                    TimeUnit.MILLISECONDS.sleep(200);
+                    TimeUnit.MILLISECONDS.sleep((long)(Math.random() * 1000));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+
+            System.out.println(" *****  <TASK-" + id + " > ENDING **********");
         }
 
     public LoopTaskA() {
