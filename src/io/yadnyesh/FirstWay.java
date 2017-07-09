f0436381f655556a24da0e5861bc36071d5dbf49
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-07-09 20:24:09 +0530

    Finishing off Sets

--- src/io/yadnyesh/FirstWay.java
+++ src/io/yadnyesh/FirstWay.java
@@ -9,13 +9,20 @@ public class FirstWay {
 
     public static void main(String[] args) {
         System.out.println("Main Thread starts here...");
+
         new FirstTask();
+        Thread t = new FirstTask();
+
         System.out.println("Main Thread ended here....");
     }
 }
 
 class FirstTask extends Thread {
 
+    private static int count = 0;
+    private int id;
+
+    @Override
     public void run() {
         for(int i = 10; i > 0; i--) {
             System.out.println("Tick Tick " + i);
@@ -28,6 +35,7 @@ class FirstTask extends Thread {
     }
 
     public FirstTask() {
+        this.id = ++count;
         this.start();
     }
 }
