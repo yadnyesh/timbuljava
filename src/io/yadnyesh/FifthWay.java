1a7a47a0205b4647d3d9ea66a9381946a2fb0445
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-07-09 21:00:16 +0530

    Section 2 Lecture 8 - Multithreading - FifthWay

--- src/io/yadnyesh/FifthWay.java
+++ src/io/yadnyesh/FifthWay.java
@@ -1,5 +1,7 @@
 package io.yadnyesh;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by z063407 on 7/9/17.
  */
@@ -8,7 +10,19 @@ public class FifthWay {
     public static void main(String[] args) {
         System.out.println("Main Thread Starts here..");
 
-
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 10; i > 0; i--) {
+                    System.out.println("Tick Tick " + i);
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
 
         System.out.println("Main Thread ends here....");
     }
