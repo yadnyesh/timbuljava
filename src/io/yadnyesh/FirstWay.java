5b09a0c34a6f23a870b19fe613a03aac676eb846
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-07-09 20:22:42 +0530

    Finishing off Sets

--- src/io/yadnyesh/FirstWay.java
+++ src/io/yadnyesh/FirstWay.java
@@ -1,12 +1,16 @@
 package io.yadnyesh;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by z063407 on 6/27/17.
  */
 public class FirstWay {
 
     public static void main(String[] args) {
+        System.out.println("Main Thread starts here...");
         new FirstTask();
+        System.out.println("Main Thread ended here....");
     }
 }
 
@@ -15,6 +19,11 @@ class FirstTask extends Thread {
     public void run() {
         for(int i = 10; i > 0; i--) {
             System.out.println("Tick Tick " + i);
+            try {
+                TimeUnit.MILLISECONDS.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
