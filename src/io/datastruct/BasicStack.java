184e63b32945f4ee861677cc7a4ab1d7dc8220ba
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-07-29 20:26:44 +0530

    Pluralsight - Java Data Strctures - Beginning Stack

---
+++ src/io/datastruct/BasicStack.java
@@ -0,0 +1,21 @@
+package io.datastruct;
+
+/**
+ * Created by z063407 on 7/29/17.
+ */
+public class BasicStack<X> {
+    private X[] data;
+    int stackPointer;
+
+    public BasicStack() {
+        data = (X[]) new Object[1000];
+    }
+
+    public void push(X newItem) {
+
+    }
+
+
+}
+
+
