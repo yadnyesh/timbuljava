d1e7d0131f48a51146ccdcc8107e092cc84d2616
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-07-29 20:34:10 +0530

    Pluralsight - Java Data Strctures - Building Stack

--- src/io/datastruct/BasicStack.java
+++ src/io/datastruct/BasicStack.java
@@ -22,4 +22,25 @@ public class BasicStack<X> {
         }
         return data[stackPointer--];
     }
+
+    public boolean contains(X item) {
+        boolean found = false;
+        for (int i = 0; i < stackPointer; i++) {
+            if(data[i].equals(item)) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+    public X access (X item) {
+        while(stackPointer > 0) {
+            X tmpItem = pop();
+            if(item.equals(tmpItem)) {
+                return tmpItem;
+            }
+        }
+        throw new IllegalArgumentException("Could not find " + item + " on the stack");
+    }
 }
