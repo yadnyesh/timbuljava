74e4083fda97d696a52db7c125114ec69b3076aa
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-09-02 09:00:02 +0530

    Pluralsight - Java Data Strctures - LinkedList Implementation

--- src/io/datastruct/BasicLinkedList.java
+++ src/io/datastruct/BasicLinkedList.java
@@ -7,15 +7,38 @@ public class BasicLinkedList<X> {
 
     private Node first;
     private Node last;
+    private int nodeCount;
 
     public BasicLinkedList() {
         first = null;
         last = null;
+        nodeCount = 0;
     }
 
     private class Node {
         private Node nextNode;
         private X nodeItem;
+
+        public Node(X item){
+            this.nextNode = null;
+            this.nodeItem = item;
+        }
+
+        public int size() {
+            return nodeCount;
+        }
+
+        public void add(X item){
+            if(first == null){
+                first = new Node(item);
+                last = first;
+            } else {
+                Node newLastNode = new Node(item);
+                last.setNextNode(newLastNode);
+                last = newLastNode;
+            }
+
+        }
 
         public Node(Node nextNode, X item) {
             this.nextNode = null;
