4ab95fa7792adc9f26ffdb0c12bb70af51c94dc5
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-09-03 11:41:17 +0530

    Naming Java Threads

---
+++ src/io/threads/NamingThreadsFirstWay.java
@@ -0,0 +1,16 @@
+package io.threads;
+
+/**
+ * Created by z063407 on 9/3/17.
+ */
+public class NamingThreadsFirstWay {
+    public static void main(String[] args) {
+        System.out.println("Main thread starts here....");
+
+
+
+        System.out.println("Main Thread ended ......");
+    }
+
+
+}
