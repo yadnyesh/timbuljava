133ee46e28dc7ddd8f6e9103fab2b78828bb1592
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-10-19 12:10:17 +0530

    Beginning Java Exception PL Sight tutorial

---
+++ src/io/exceptionhandling/First.java
@@ -0,0 +1,11 @@
+package io.exceptionhandling;
+
+/**
+ * Created by z063407 on 10/19/17.
+ */
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println(new Exception());
+    }
+}
