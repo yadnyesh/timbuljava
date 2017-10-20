e49ef4f647a43fa64e6c7b819a3c4ee67ab91b89
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-10-20 08:30:31 +0530

    working on Exception Handling

--- src/io/exceptionhandling/First.java
+++ src/io/exceptionhandling/First.java
@@ -7,5 +7,12 @@ public class First {
 
     public static void main(String[] args) {
         System.out.println(new Exception());
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.print("Caught!");
+        } finally {
+            System.out.print("Finally!");
+        }
     }
 }
