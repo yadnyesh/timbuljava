4b718d1bb2efae9529b60119c823568a17e0ac7e
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-11-22 05:15:02 +0530

    Initial Circular Queue Setup

--- src/io/datastruct/Queue.java
+++ src/io/datastruct/Queue.java
@@ -1,5 +1,18 @@
 package io.datastruct;
 
-public class Queue {
-    
+
+import java.lang.reflect.Array;
+
+public class Queue<T> {
+
+    private static final int SPECIAL_EMPTY_VALUE = -1;
+    private static int MAX_SIZE = 40;
+    private T[] elements;
+
+    private int headIndex = SPECIAL_EMPTY_VALUE;
+    private int tailIndex = SPECIAL_EMPTY_VALUE;
+
+    public Queue(Class<T> clazz) {
+        elements = (T[]) Array.newInstance(clazz, MAX_SIZE);
+    }
 }
