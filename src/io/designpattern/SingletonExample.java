52f202d118858f182c3f932e9e832d3b041586e8
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-12-29 18:51:00 +0530

    Singleton Design Pattern

--- src/io/designpattern/SingletonExample.java
+++ src/io/designpattern/SingletonExample.java
@@ -7,5 +7,9 @@ public class SingletonExample {
 	
 	public static void main(String[] args) {
 	
+		Singleton instance1 = Singleton.getInstance();
+		System.out.println(instance1);
+		Singleton instance2 = Singleton.getInstance();
+		System.out.println(instance2);
 	}
 }
