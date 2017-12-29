79033314c693eb8f9a41cd883b2c32a640ce9b4b
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2017-12-29 18:47:12 +0530

    Singleton Design Pattern

---
+++ src/io/designpattern/Singleton.java
@@ -0,0 +1,37 @@
+package io.designpattern;
+
+/**
+ * Created by z063407 on 12/27/17.
+ */
+public class Singleton {
+	
+	private static volatile Singleton instance = null;
+	
+	private Singleton() {
+	
+	}
+	
+	private static Singleton getInstance() {
+		
+		if(instance == null){
+			synchronized(Singleton.class) {
+				if(instance == null){
+					instance = new Singleton();
+				}
+			}
+		}
+		
+		return instance;
+	}
+	
+}
+
+
+/***
+ # Singleton
+ - Static in nature
+ - Thread Safe
+ - Private Constructor
+ - Private Instance of class
+ - No parameters to the constructor
+***/
---
+++ src/io/designpattern/SingletonExample.java
@@ -0,0 +1,11 @@
+package io.designpattern;
+
+/**
+ * Created by z063407 on 12/27/17.
+ */
+public class SingletonExample {
+	
+	public static void main(String[] args) {
+	
+	}
+}
