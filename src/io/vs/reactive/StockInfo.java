8ffb06d802816ab0925e910afd4574a57c72f900
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-01-01 20:51:40 +0530

    Reactive programming - Added YahooFinance code

--- src/io/vs/reactive/StockInfo.java
+++ src/io/vs/reactive/StockInfo.java
@@ -1,8 +1,26 @@
 package io.vs.reactive;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
  * Created by z063407 on 1/1/18.
  */
 public class StockInfo {
-
+	public final String ticker;
+	public final BigDecimal value;
+	
+	public StockInfo(String ticker, BigDecimal value) {
+		this.ticker = ticker;
+		this.value = value;
+	}
+	
+	public static StockInfo fetch(String ticker){
+		return new StockInfo(ticker, YahooFinance.getPrice(ticker));
+	}
+	
+	@Override
+	public String toString() {
+		return "StockInfo{" + "ticker='" + ticker + '\'' + ", value=" + value + '}';
+	}
 }
--- src/io/vs/reactive/StockServer.java
+++ src/io/vs/reactive/StockServer.java
@@ -11,6 +11,21 @@ import java.util.List;
 public class StockServer {
 	
 	public static Observable<StockInfo> getFeed(List<String> symbols) {
-		return null;
+		return Observable.create(subscriber -> {
+			while(true){
+				symbols.stream()
+						.map(StockInfo::fetch)
+						.forEach(subscriber::onNext);
+				sleep(1000);
+			}
+		});
+	}
+	
+	private static void sleep(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch(InterruptedException e){
+			e.printStackTrace();
+		}
 	}
 }
