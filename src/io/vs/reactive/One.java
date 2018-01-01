46e477c818fa1ed98a7a96aa9ce5b9aee332237c
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-01-01 20:17:49 +0530

    Stream of numbers

--- src/io/vs/reactive/One.java
+++ src/io/vs/reactive/One.java
@@ -1,6 +1,7 @@
 package io.vs.reactive;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by z063407 on 1/1/18.
@@ -14,6 +15,9 @@ public class One {
 	}
 	
 	public static void main(String[] args) {
-
+		Stream.iterate(100, e -> e + 1)
+				.filter(One::isPrime)
+				.limit(10)
+				.forEach(System.out::println);
 	}
 }
