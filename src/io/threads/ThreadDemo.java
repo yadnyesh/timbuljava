fbf024a37c30a853ddac150b4291035c09072bfe
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-11 15:44:56 +0530

    o/p with t.run()

--- src/io/threads/ThreadDemo.java
+++ src/io/threads/ThreadDemo.java
@@ -4,7 +4,7 @@ public class ThreadDemo {
 	
 	public static void main(String[] args) {
 		MyThread t = new MyThread(); //thread instantiation
-		t.start(); //Main thread starts child thread
+		t.run(); //Main thread starts child thread
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Main Thread");
 		}
