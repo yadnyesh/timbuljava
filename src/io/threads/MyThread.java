d218dee8c7b8c8cda210a5ced3788d448e477351
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-13 13:42:15 +0530

    Thread Yield method

--- src/io/threads/MyThread.java
+++ src/io/threads/MyThread.java
@@ -5,6 +5,7 @@ public class MyThread extends Thread{
 	public void run() {
 		for (int i=0; i < 10; i++) {
 			System.out.println("Child Thread");
+			Thread.yield();
 			//System.out.println(Thread.currentThread());
 		}
 	}
