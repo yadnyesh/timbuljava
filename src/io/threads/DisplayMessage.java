3e1dedae9240ec57d41744b4232e95a293a869df
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-16 14:04:38 +0530

    Without Synchronized

--- src/io/threads/DisplayMessage.java
+++ src/io/threads/DisplayMessage.java
@@ -2,7 +2,7 @@ package io.threads;
 
 public class DisplayMessage {
 	
-	public synchronized void wish(String name) {
+	public void wish(String name) {
 		int t = 0;
 		for (int i = 0 ; i < 10; i++){
 			System.out.println("Hello: " + name + " t is, :" + t);
