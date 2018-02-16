fda6156494c3e2e49c8ed53bdffa3f2a365a28dd
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-16 19:29:49 +0530

    Created new class to display Characters

--- src/io/threads/ThreadDisplayChar.java
+++ src/io/threads/ThreadDisplayChar.java
@@ -1,5 +1,13 @@
 package io.threads;
 
 public class ThreadDisplayChar extends Thread {
-
+	Display d;
+	
+	ThreadDisplayChar(Display d){
+		this.d = d;
+	}
+	
+	public void run(){
+		d.displayc();
+	}
 }
