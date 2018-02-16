9a8afec6430fd1b4f0405cd0e4ee926b6038073e
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-16 12:30:44 +0530

    Adding sleep method to the thread.

--- src/io/threads/DisplayMessage.java
+++ src/io/threads/DisplayMessage.java
@@ -5,6 +5,12 @@ public class DisplayMessage {
 	public void wish(String name) {
 		for (int i = 0 ; i < 10; i++){
 			System.out.println("Hello: " + name);
+			try{
+				Thread.sleep(2000);
+			}catch (InterruptedException e){
+				e.printStackTrace();
+			}
 		}
+		
 	}
 }
