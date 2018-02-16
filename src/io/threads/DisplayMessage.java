abe49b0b755f943627db16d72086656b7bd804b7
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-16 14:04:13 +0530

    With Synchronized

--- src/io/threads/DisplayMessage.java
+++ src/io/threads/DisplayMessage.java
@@ -3,8 +3,10 @@ package io.threads;
 public class DisplayMessage {
 	
 	public synchronized void wish(String name) {
+		int t = 0;
 		for (int i = 0 ; i < 10; i++){
-			System.out.println("Hello: " + name);
+			System.out.println("Hello: " + name + " t is, :" + t);
+			t = t + 1;
 			try{
 				Thread.sleep(2000);
 			}catch (InterruptedException e){
