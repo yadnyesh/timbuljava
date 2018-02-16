1f59b60d4cc37eb53d93acaf6a6abba0742199da
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-16 19:58:16 +0530

    Defining the two objects calling the same thread

--- src/io/threads/AnotherSynchronizeDemo.java
+++ src/io/threads/AnotherSynchronizeDemo.java
@@ -1,5 +1,10 @@
 package io.threads;
 
 public class AnotherSynchronizeDemo {
-
+	
+	public static void main(String[] args) {
+		Display d = new Display();
+		ThreadDisplay tn = new ThreadDisplay(d);
+		ThreadDisplayChar tc = new ThreadDisplayChar(d);
+	}
 }
