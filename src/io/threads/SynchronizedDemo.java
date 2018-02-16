87a8e8b528d3d05c92091df9e178a92398d51655
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-16 13:52:11 +0530

    Creating 2 threads

--- src/io/threads/SynchronizedDemo.java
+++ src/io/threads/SynchronizedDemo.java
@@ -5,6 +5,8 @@ public class SynchronizedDemo {
 	public static void main(String[] args) {
 		DisplayMessage d = new DisplayMessage();
 		MyThread t1 = new MyThread(d, "Dhoni");
+		MyThread t2 = new MyThread(d, "Yogi");
 		t1.start();
+		t2.start();
 	}
 }
