7f7b44013bcd306cef9d0ed92ed1670e5a6556d1
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-16 13:46:50 +0530

    Created intermediate class

--- src/io/threads/MyThread.java
+++ src/io/threads/MyThread.java
@@ -2,10 +2,23 @@ package io.threads;
 
 public class MyThread extends Thread{
 	
-	public void run() {
-		for (int i=0; i < 10; i++) {
-			System.out.println("Child Thread");
-			Thread.yield();
-		}
+	DisplayMessage d;
+	String name;
+	
+	MyThread(DisplayMessage d, String name) {
+		this.d = d;
+		this.name = name;
 	}
+	
+	public void run(){
+		d.wish(name);
+	}
+	
+	
+//	public void run() {
+//		for (int i=0; i < 10; i++) {
+//			System.out.println("Child Thread");
+//			Thread.yield();
+//		}
+//	}
 }
