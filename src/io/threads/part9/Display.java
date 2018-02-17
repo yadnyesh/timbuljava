4471d99dfb06ac262ae261afe9077004744a4e59
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-17 16:39:28 +0530

    Created a block

--- src/io/threads/part9/Display.java
+++ src/io/threads/part9/Display.java
@@ -2,5 +2,15 @@ package io.threads.part9;
 
 public class Display {
 
-
+	public void wish(String name){
+		for (int i =0; i <=10 ; i++){
+			System.out.println("Hey...");
+			try {
+				Thread.sleep(1000);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(name);
+		}
+	}
 }
