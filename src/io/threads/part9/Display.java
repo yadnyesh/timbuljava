5cb7990678c8275303f122abf5cdc4ee8c9194a6
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-17 16:43:52 +0530

    Added the synchronized block

--- src/io/threads/part9/Display.java
+++ src/io/threads/part9/Display.java
@@ -5,12 +5,14 @@ public class Display {
 	public void wish(String name){
 		for (int i =0; i <=10 ; i++){
 			System.out.println("Hey...");
-			try {
-				Thread.sleep(1000);
-			} catch(InterruptedException e) {
-				e.printStackTrace();
+			synchronized(this) {
+				try {
+					Thread.sleep(1000);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(name);
 			}
-			System.out.println(name);
 		}
 	}
 }
