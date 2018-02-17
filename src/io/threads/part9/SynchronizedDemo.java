2787a6ff3793bb8c1a1f34c99b39331917a88318
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-17 16:42:31 +0530

    Added some code

--- src/io/threads/part9/SynchronizedDemo.java
+++ src/io/threads/part9/SynchronizedDemo.java
@@ -3,6 +3,8 @@ package io.threads.part9;
 public class SynchronizedDemo {
 	
 	public static void main(String[] args) {
-	
+		Display d = new Display();
+		MyThread td = new MyThread(d, "dhoni");
+		MyThread tv = new MyThread(d, "Kohli");
 	}
 }
