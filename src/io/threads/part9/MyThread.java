d59b8791b6d2855cefa0e92e826ca250f1d745bf
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-17 16:40:57 +0530

    Completed - creating the MyThread class

--- src/io/threads/part9/MyThread.java
+++ src/io/threads/part9/MyThread.java
@@ -1,3 +1,15 @@
 package io.threads.part9;
 
-public class MyThread {}
+public class MyThread extends Thread {
+	Display d;
+	String name;
+	
+	MyThread(Display d, String name){
+		this.d = d;
+		this.name = name;
+	}
+	
+	public void run(){
+		d.wish(name);
+	}
+}
