348337e2027a10c4b13851ad3ca7230de534a19d
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-17 16:48:26 +0530

    Garbled output due to multiple thread exection

--- src/io/threads/part9/SynchronizedDemo.java
+++ src/io/threads/part9/SynchronizedDemo.java
@@ -3,9 +3,10 @@ package io.threads.part9;
 public class SynchronizedDemo {
 	
 	public static void main(String[] args) {
-		Display d = new Display();
-		MyThread td = new MyThread(d, "dhoni");
-		MyThread tv = new MyThread(d, "Kohli");
+		Display dt = new Display();
+		Display dv = new Display();
+		MyThread td = new MyThread(dt, "dhoni");
+		MyThread tv = new MyThread(dv, "Kohli");
 		td.start();
 		tv.start();
 	}
