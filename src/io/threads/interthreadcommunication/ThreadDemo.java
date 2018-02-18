894156677e015adab7dfa2d9099211cb8b1b9f50
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-18 19:21:24 +0530

    Wait Notify - with timeout.

--- src/io/threads/interthreadcommunication/ThreadDemo.java
+++ src/io/threads/interthreadcommunication/ThreadDemo.java
@@ -5,9 +5,10 @@ public class ThreadDemo {
 	public static void main(String[] args) throws InterruptedException{
 		ThreadA b = new ThreadA();
 		b.start();
+		Thread.sleep(1000);
 		synchronized(b){
 			System.out.println("Main Thread calling wait method.....");
-			b.wait();
+			b.wait(1000);
 			System.out.println(b.total);
 		}
 	}
