5c3099f96d772014195c3fcc5fd77073e58901f7
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-18 18:54:10 +0530

    Complete - Creating the Thread class

--- src/io/threads/interthreadcommunication/ThreadA.java
+++ src/io/threads/interthreadcommunication/ThreadA.java
@@ -3,4 +3,9 @@ package io.threads.interthreadcommunication;
 public class ThreadA extends Thread {
 	int total = 0;
 	
+	public void run() {
+		for (int i = 1; i <= 100;i ++) {
+			total = total + i;
+		}
+	}
 }
