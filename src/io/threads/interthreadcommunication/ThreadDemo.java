54830f53785d9bfc0600c9cf78797425f9bfa8af
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-18 18:57:06 +0530

    Join method introduced to ensure completion of thread A before Main thread

--- src/io/threads/interthreadcommunication/ThreadDemo.java
+++ src/io/threads/interthreadcommunication/ThreadDemo.java
@@ -5,7 +5,7 @@ public class ThreadDemo {
 	public static void main(String[] args) throws InterruptedException{
 		ThreadA b = new ThreadA();
 		b.start();
-		Thread.sleep(1);
+		b.join(); //Wait till ThreadA is completed
 		System.out.println(b.total);
 	}
 }
