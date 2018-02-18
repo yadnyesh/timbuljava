f96dddcd8ad8cd3a4f05e6180332d2a7c970840c
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-18 18:56:38 +0530

    Sleep method introduced to ensure completion of thread A before Main thread

--- src/io/threads/interthreadcommunication/ThreadDemo.java
+++ src/io/threads/interthreadcommunication/ThreadDemo.java
@@ -2,7 +2,10 @@ package io.threads.interthreadcommunication;
 
 public class ThreadDemo {
 	
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws InterruptedException{
+		ThreadA b = new ThreadA();
+		b.start();
+		Thread.sleep(1);
+		System.out.println(b.total);
 	}
 }
