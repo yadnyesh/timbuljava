591c9273944524613d2a24e953f6a95ee9328b60
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-20 13:58:37 +0530

    Printing IntStream

--- src/io/streams/JavaStreams.java
+++ src/io/streams/JavaStreams.java
@@ -1,5 +1,13 @@
 package io.streams;
 
-public class JavaStreams {
+import java.util.stream.IntStream;
 
+public class JavaStreams {
+	
+	public static void main(String[] args) {
+		IntStream
+				.range(1,10)
+				.forEach(System.out::print);
+		System.out.println();
+	}
 }
