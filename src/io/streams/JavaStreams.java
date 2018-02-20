b442a7d8f6f4bbe199b13bf35edb8b2501928b10
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-20 14:11:13 +0530

    Stream of Strings

--- src/io/streams/JavaStreams.java
+++ src/io/streams/JavaStreams.java
@@ -1,21 +1,15 @@
 package io.streams;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class JavaStreams {
 	
 	public static void main(String[] args) {
-		
-		System.out.println(
-				IntStream
-				.range(1,5)
-				.sum()
-		);
-		
-//		IntStream
-//				.range(1,10)
-//				.skip(5)
-//				.forEach(x -> System.out.println(x));
-//		System.out.println();
+		Stream.of("Yadnyesh", "Bharat", "Juvekar")
+				.sorted()
+				.findFirst()
+				.ifPresent(System.out::println);
+				
 	}
 }
