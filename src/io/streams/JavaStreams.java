e60493ef638d36135b6ec1ea4f945cff1e3db146
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-20 14:09:21 +0530

    Added code in SOP statement

--- src/io/streams/JavaStreams.java
+++ src/io/streams/JavaStreams.java
@@ -5,10 +5,17 @@ import java.util.stream.IntStream;
 public class JavaStreams {
 	
 	public static void main(String[] args) {
-		IntStream
-				.range(1,10)
-				.skip(5)
-				.forEach(x -> System.out.println(x));
-		System.out.println();
+		
+		System.out.println(
+				IntStream
+				.range(1,5)
+				.sum()
+		);
+		
+//		IntStream
+//				.range(1,10)
+//				.skip(5)
+//				.forEach(x -> System.out.println(x));
+//		System.out.println();
 	}
 }
