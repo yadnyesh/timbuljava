11d2cb298345930bf56d5285970d14f6602fbcf1
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-20 14:27:45 +0530

    Process stream of arrays

--- src/io/streams/JavaStreams.java
+++ src/io/streams/JavaStreams.java
@@ -1,14 +1,16 @@
 package io.streams;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class JavaStreams {
 	
 	public static void main(String[] args) {
-		Stream.of("Yadnyesh", "Bharat", "Juvekar")
-				.sorted()
-				.findFirst()
+
+		Arrays.stream(new int[]{1,2,3,4,5,6})
+				.map(x -> x*x)
+				.average()
 				.ifPresent(System.out::println);
 				
 	}
