08dbdeb9f88fd605d8f66cddd5e7f3f7ab4dbd1c
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-21 18:44:08 +0530

    Adding elements to collections

--- src/io/collections/HashSetProgram.java
+++ src/io/collections/HashSetProgram.java
@@ -1,5 +1,19 @@
 package io.collections;
 
-public class HashSetProgram {
+import java.util.HashSet;
 
+public class HashSetProgram {
+	
+	public static void main(String[] args) {
+		HashSet h = new HashSet();
+		h.add("B");
+		h.add("C");
+		h.add("D");
+		h.add("E");
+		h.add("F");
+		h.add("G");
+		
+	}
+	
+	
 }
