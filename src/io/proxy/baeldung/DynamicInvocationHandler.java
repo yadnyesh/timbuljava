e788fd4e591ca40694dc47f41565e069a7a78b8b
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-02-28 07:46:37 +0530

    Created new Class to study proxy

---
+++ src/io/proxy/baeldung/DynamicInvocationHandler.java
@@ -0,0 +1,18 @@
+package io.proxy.baeldung;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+
+public class DynamicInvocationHandler implements InvocationHandler {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(DynamicInvocationHandler.class);
+	
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		return null;
+	}
+}
