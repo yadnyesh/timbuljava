7053b4f0ea1d24c86fbafb22198f50665f8851b7
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-03-25 20:37:50 +0530

    Implemented - MapInterfaceExample

--- src/io/collections/MapInterfaceExample.java
+++ src/io/collections/MapInterfaceExample.java
@@ -1,3 +1,15 @@
 package io.collections;
 
-public class MapInterfaceExample {}
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapInterfaceExample {
+	
+	public static void main(String[] args) {
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(100, "Yadnyesh");
+		map.put(101, "Yadnyesh - 101");
+		map.put(102, "Yadnyesh - 102");
+		map.keySet().stream().forEach(System.out::println);
+	}
+}
