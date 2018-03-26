7db71676ce4a02cf5e564a60ad59954923eaebac
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-03-26 14:00:53 +0530

    Created ArrayDequeExample - Simple Example

--- src/io/collections/durga/ArrayDequeExample.java
+++ src/io/collections/durga/ArrayDequeExample.java
@@ -1,5 +1,18 @@
 package io.collections.durga;
 
-public class ArrayDequeExample {
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+public class ArrayDequeExample {
+	
+	public static void main(String[] args) {
+		Deque<String> myDeque = new ArrayDeque<>();
+		myDeque.add("One");
+		myDeque.add("Two");
+		myDeque.add("Three");
+		myDeque.add("Four");
+		
+		myDeque.stream().forEach(System.out::println);
+		
+	}
 }
