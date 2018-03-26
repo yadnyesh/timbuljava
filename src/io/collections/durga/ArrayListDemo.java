0148c89d544f6bc1ef58f93aea0757417773de5c
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-03-26 07:53:29 +0530

    Implementation of ArrayList

--- src/io/collections/durga/ArrayListDemo.java
+++ src/io/collections/durga/ArrayListDemo.java
@@ -3,5 +3,18 @@ package io.collections.durga;
 import java.util.ArrayList;
 
 public class ArrayListDemo {
-
+	public static void main(String[] args) {
+		ArrayList l = new ArrayList();
+		l.add("A");
+		l.add(10);
+		l.add("A");
+		l.add(null);
+		l.stream().forEach(System.out::println);
+		l.remove(2);
+		l.add(2, "M");
+		l.add("N");
+		l.stream().forEach(System.out::println);
+		System.out.println("============");
+		System.out.println(l);
+	}
 }
