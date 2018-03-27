7c22b344b7a2ef4def4777acec6ea8588ae51680
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-03-27 12:26:38 +0530

    Sample program for PriorityQueueExample with String

--- src/io/collections/PriorityQueueExample.java
+++ src/io/collections/PriorityQueueExample.java
@@ -1,5 +1,17 @@
 package io.collections;
 
+import java.util.PriorityQueue;
+
 public class PriorityQueueExample {
+	
+	public static void main(String[] args) {
+		PriorityQueue<String> pqueue = new PriorityQueue<String>();
+		pqueue.add("India");
+		pqueue.add("Pakistan");
+		pqueue.add("Bangladesh");
+		pqueue.stream().forEach(System.out::println);
+	}
+	
+	
 
 }
