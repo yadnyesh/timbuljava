28e63f2573fa0a6c65dbb654fb93cbd8daddfd52
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-03-27 14:34:24 +0530

    Created the Book class

--- src/io/collections/PriorityQueueExample.java
+++ src/io/collections/PriorityQueueExample.java
@@ -5,6 +5,36 @@ import java.util.Queue;
 
 public class PriorityQueueExample {
 	
+	class Book {
+		private int id;
+		private String name;
+		private String author;
+		private String publisher;
+		int quantity;
+		
+		public Book(){
+		
+		}
+		
+		public Book(int id, String name, String author, String publisher, int quantity) {
+			this.id = id;
+			this.name = name;
+			this.author = author;
+			this.publisher = publisher;
+			this.quantity = quantity;
+		}
+		
+		@Override
+		public String toString() {
+			return "Book{" + "id=" + id + ", name='" + name + '\'' + ", author='" + author + '\'' + ", publisher='" + publisher + '\'' + ", quantity=" + quantity + '}';
+		}
+		
+		public int compareTo(Book book){
+			if (this.id > book.id);
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		Queue<String> pqueue = new PriorityQueue<String>();
 		pqueue.add("India");
