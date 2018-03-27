55d8ca767b66dc9a6f8cdb7ce25c3bd0faeec8de
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-03-27 13:22:07 +0530

    Program to Interface with Queue

--- src/io/collections/PriorityQueueExample.java
+++ src/io/collections/PriorityQueueExample.java
@@ -1,14 +1,18 @@
 package io.collections;
 
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class PriorityQueueExample {
 	
 	public static void main(String[] args) {
-		PriorityQueue<String> pqueue = new PriorityQueue<String>();
+		Queue<String> pqueue = new PriorityQueue<String>();
 		pqueue.add("India");
 		pqueue.add("Pakistan");
 		pqueue.add("Bangladesh");
+		pqueue.add("Srilanka");
+		pqueue.add("West Indies");
+		pqueue.add("England");
 		pqueue.stream().forEach(System.out::println);
 	}
 	
