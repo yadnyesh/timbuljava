51c8099c1f5b33475cdf53d2f15b8459252a3c21
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-03-27 19:55:21 +0530

    Created a function

--- src/io/functional/in28minutes/FunctionalProgrammingRunner.java
+++ src/io/functional/in28minutes/FunctionalProgrammingRunner.java
@@ -1,5 +1,17 @@
 package io.functional.in28minutes;
 
-public class FunctionalProgrammingRunner {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class FunctionalProgrammingRunner {
+	
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Apple", "Banana", "Cat", "Dog");
+		printBasic(list);
+	}
+	
+	private static void printBasic(List<String> list) {
+		list.stream().forEach(System.out::println);
+	}
 }
