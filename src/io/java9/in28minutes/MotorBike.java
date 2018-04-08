5e66e9a06413f6fbf9f4720595d8849a62c21888
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-08 10:25:49 +0530

    Created MotorBike class

---
+++ src/io/java9/in28minutes/MotorBike.java
@@ -0,0 +1,18 @@
+package io.java9.in28minutes;
+
+public class MotorBike {
+	
+	private int speed;
+	
+	MotorBike(int speed) {
+		this.speed =speed;
+	}
+	
+	public int getSpeed(){
+		return speed;
+	}
+	
+	public void setSpeed(int speed){
+		this.speed = speed;
+	}
+}
