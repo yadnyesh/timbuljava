2cfe75bdee7e57a5689c72acb0030b500242e92d
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-08 12:49:01 +0530

    Test Main Method

--- src/io/java9/in28minutes/BiNumberRunner.java
+++ src/io/java9/in28minutes/BiNumberRunner.java
@@ -1,5 +1,13 @@
 package io.java9.in28minutes;
 
-public class BiNumberRunner {
+import java.sql.SQLOutput;
 
+public class BiNumberRunner {
+	
+	
+	public static void main(String[] args) {
+		BiNumber numbers = new BiNumber(4,5);
+		System.out.println(numbers.add());
+		System.out.println(numbers.multiply());
+	}
 }
