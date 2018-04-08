0b931657efa74fa2c9306c704cbca1e399eb5394
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-08 10:38:05 +0530

    Created MotorBikeRunner

--- src/io/java9/in28minutes/MotorBikeRunner.java
+++ src/io/java9/in28minutes/MotorBikeRunner.java
@@ -3,6 +3,10 @@ package io.java9.in28minutes;
 public class MotorBikeRunner {
 	
 	public static void main(String[] args) {
-	
+		MotorBike ducati = new MotorBike(100);
+		MotorBike honda = new MotorBike(200);
+		
+		System.out.println(ducati.getSpeed());
+		System.out.println(honda.getSpeed());
 	}
 }
