80806029b32d60fbc3a4eb13ed20326c9494e942
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-08 12:47:36 +0530

    Updated BiNumber

--- src/io/java9/in28minutes/BiNumber.java
+++ src/io/java9/in28minutes/BiNumber.java
@@ -17,4 +17,20 @@ public class BiNumber {
 	public int multiply() {
 		return number1 * number2;
 	}
+	
+	public int getNumber1() {
+		return number1;
+	}
+	
+	public void setNumber1(int number1) {
+		this.number1 = number1;
+	}
+	
+	public int getNumber2() {
+		return number2;
+	}
+	
+	public void setNumber2(int number2) {
+		this.number2 = number2;
+	}
 }
