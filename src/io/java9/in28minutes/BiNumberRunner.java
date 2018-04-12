a49698a9b46c3fb9170b581297d5f40a62e69af5
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-12 11:20:34 +0530

    Added the Employee class

--- src/io/java9/in28minutes/BiNumberRunner.java
+++ src/io/java9/in28minutes/BiNumberRunner.java
@@ -4,7 +4,6 @@ import java.sql.SQLOutput;
 
 public class BiNumberRunner {
 	
-	
 	public static void main(String[] args) {
 		BiNumber numbers = new BiNumber(4,5);
 		System.out.println(numbers.add());
---
+++ src/io/ttp/Employee.java
@@ -0,0 +1,12 @@
+package io.ttp;
+
+public class Employee {
+	public String name;
+	public String address;
+	public transient int SSN;
+	public int number;
+	
+	public void mailCheck() {
+		System.out.println("Mailing a check to " + name + "  " + address);
+	}
+}
