73a2a2f45ff74f6b0dcc8fcce583041117e4baac
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-12 11:34:33 +0530

    Created the class to demo Serialize

--- src/io/ttp/SerializeDemo.java
+++ src/io/ttp/SerializeDemo.java
@@ -1,5 +1,20 @@
 package io.ttp;
 
-public class SerializeDemo {
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class SerializeDemo {
+	
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.name = "Yadnyesh";
+		e.address = "Goa";
+		e.SSN = 12314;
+		e.number = 101;
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream("employee.ser");
+		} catch (IOException e){
+			System.out.println(e);
+		}
+	}
 }
