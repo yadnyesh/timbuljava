cea9c35f2820277d388f404b85177bdb0796c0dd
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-13 22:49:21 +0530

    Create Student object

--- src/io/java9/in28minutes/StudentRunner.java
+++ src/io/java9/in28minutes/StudentRunner.java
@@ -1,5 +1,10 @@
 package io.java9.in28minutes;
 
 public class StudentRunner {
-
+	
+	public static void main(String[] args) {
+		int[] marks = {98, 99, 100};
+		
+		Student student = new Student("Yadnyesh", marks);
+	}
 }
