c260024fa46bbcab0dbd68acfffd8acb5e02f3cf
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-14 15:05:39 +0530

    Created Student Class

--- src/io/java9/in28minutes/collections/Student.java
+++ src/io/java9/in28minutes/collections/Student.java
@@ -3,4 +3,33 @@ package io.java9.in28minutes.collections;
 public class Student {
 	private int id;
 	private String name;
+	
+	public Student() {
+	}
+	
+	public Student(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student{" + "id=" + id + ", name='" + name + '\'' + '}';
+	}
 }
