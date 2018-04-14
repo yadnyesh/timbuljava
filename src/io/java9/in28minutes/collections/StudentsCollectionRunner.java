0041ed283f8895441b0047a9636d9c1ae9a82daa
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-14 15:07:19 +0530

    Created StudentRunner Class

--- src/io/java9/in28minutes/collections/StudentsCollectionRunner.java
+++ src/io/java9/in28minutes/collections/StudentsCollectionRunner.java
@@ -1,3 +1,13 @@
 package io.java9.in28minutes.collections;
 
-public class StudentsCollectionRunner {}
+import java.util.List;
+
+public class StudentsCollectionRunner {
+	
+	public static void main(String[] args) {
+		List<Student> studentList = List.of(new Student(1,"Yadnyesh"),
+											new Student(2, "Name 2"),
+											new Student(3, "3 Name"));
+		
+	}
+}
