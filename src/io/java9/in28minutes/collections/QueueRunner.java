1c5b163c5f421458b05250174bb8c823cef4155e
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-14 19:17:51 +0530

    queue demo

--- src/io/java9/in28minutes/collections/QueueRunner.java
+++ src/io/java9/in28minutes/collections/QueueRunner.java
@@ -1,5 +1,24 @@
 package io.java9.in28minutes.collections;
 
-public class QueueRunner {
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+class StringLengthComparator implements Comparator<String> {
+	
+	@Override
+	public int compare(String string1, String string2) {
+		return Integer.compare(string1.length(), string2.length());
+	}
+}
+
+
+public class QueueRunner {
+	
+	public static void main(String[] args) {
+		Queue<String> queue = new PriorityQueue<>(new StringLengthComparator());
+		queue.addAll(List.of("Zebra", "Monkey", "Cat"));
+		System.out.println(queue);
+	}
 }
