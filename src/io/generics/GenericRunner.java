7f86c7be4ed522c64bf00db90a59e37f6e57ca68
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-15 11:38:54 +0530

    Generic static method implementation

--- src/io/generics/GenericRunner.java
+++ src/io/generics/GenericRunner.java
@@ -1,9 +1,19 @@
 package io.generics;
 
+import java.util.List;
+
 public class GenericRunner {
 	
+	static double sumOfNumberList(List<? extends Number> numbers) {
+		double value = 0.0;
+		for(Number number1:numbers){
+			value += number1.doubleValue();
+		}
+		return value;
+	}
 	
 	public static void main(String[] args) {
+		
 		MyCustomList<String> list = new MyCustomList<>();
 		list.addElement("Yadnyesh");
 		System.out.println(list);
@@ -11,5 +21,6 @@ public class GenericRunner {
 		MyCustomList<Integer> intlist = new MyCustomList<>();
 		intlist.addElement(Integer.valueOf(1));
 		System.out.println(intlist);
+		System.out.println(sumOfNumberList(List.of(1,2,3,4,5)));
 	}
 }
