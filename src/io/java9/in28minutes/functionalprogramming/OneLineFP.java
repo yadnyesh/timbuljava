6855da2292411177431a3e092c34e2d71ed0b115
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-15 19:05:41 +0530

    Filter and Print max Integer

--- src/io/java9/in28minutes/functionalprogramming/OneLineFP.java
+++ src/io/java9/in28minutes/functionalprogramming/OneLineFP.java
@@ -1,5 +1,13 @@
 package io.java9.in28minutes.functionalprogramming;
 
-public class OneLineFP {
+import java.util.List;
 
+public class OneLineFP {
+	
+	public static void main(String[] args) {
+		System.out.println(List.of(23,45,67,12)
+				.stream()
+				.filter(n -> n%2==0)
+				.max((n1,n2) -> Integer.compare(n1,n2)).get());
+	}
 }
