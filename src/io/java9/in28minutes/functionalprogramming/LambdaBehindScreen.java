e788ea92bb40124f5fdbf9e5d7a52a50e34654dc
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-15 22:17:58 +0530

    Method References

--- src/io/java9/in28minutes/functionalprogramming/LambdaBehindScreen.java
+++ src/io/java9/in28minutes/functionalprogramming/LambdaBehindScreen.java
@@ -2,6 +2,7 @@ package io.java9.in28minutes.functionalprogramming;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer>{
@@ -20,16 +21,26 @@ class SystemOutConsumer implements Consumer<Integer>{
 	}
 }
 
+class NumberSquareStream implements Function<Integer, Integer>{
+	
+	@Override
+	public Integer apply(Integer integer) {
+		return integer * integer;
+	}
+}
+
 public class LambdaBehindScreen {
 	
 	public static void main(String[] args) {
 		List.of(23,45,67,12, 36, 48, 22)
 				.stream()
 				.filter(n -> n%2==0)
+				.map(n -> n* n)
 				.forEach(System.out::println);
 		System.out.println("My Implementation now...");
 		List.of(23,45,67,12, 36, 48, 22)
 				.stream()
+				.map(new NumberSquareStream())
 				.filter(new EvenNumberPredicate())
 				.forEach(new SystemOutConsumer());
 		
