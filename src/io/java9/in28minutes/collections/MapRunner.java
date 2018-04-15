44320faab2402d23e0519e95f9edd1f70ca64e45
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-15 07:47:26 +0530

    Map implementation

---
+++ src/io/java9/in28minutes/collections/MapRunner.java
@@ -0,0 +1,13 @@
+package io.java9.in28minutes.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapRunner {
+	
+	public static void main(String[] args) {
+		String str = "This is an awesome occasion. This has never happened before";
+		Map<Character, Integer> occurances = new HashMap<>();
+		occurances.put()
+	}
+}
--- src/io/java9/in28minutes/collections/QueueRunner.java
+++ src/io/java9/in28minutes/collections/QueueRunner.java
@@ -8,7 +8,7 @@ import java.util.Queue;
 class StringLengthComparator implements Comparator<String> {
 	
 	@Override
-	public int compare(String string1, String string2) {
+	public int compare(String string1, String string2) { 
 		return Integer.compare(string1.length(), string2.length());
 	}
 }
