c803fd670768527186cb1cd8f9e02069bcf964ab
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-15 07:51:20 +0530

    Map implementation with Character occurences

--- src/io/java9/in28minutes/collections/MapRunner.java
+++ src/io/java9/in28minutes/collections/MapRunner.java
@@ -8,6 +8,18 @@ public class MapRunner {
 	public static void main(String[] args) {
 		String str = "This is an awesome occasion. This has never happened before";
 		Map<Character, Integer> occurances = new HashMap<>();
-		occurances.put()
+		char[] chars = str.toCharArray();
+		for(char character:chars){
+			Integer integer = occurances.get(character);
+			if(integer == null){
+				occurances.put(character,1);
+			} else {
+				occurances.put(character,integer+1);
+			}
+		}
+		System.out.println(occurances);
+		
+		
+		
 	}
 }
