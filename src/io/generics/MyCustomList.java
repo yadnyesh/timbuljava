28887dd9105bcb39e1ce4bd201c6ee6fbd2dec17
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-15 11:16:35 +0530

    Created MyCustomList for Generics

--- src/io/generics/MyCustomList.java
+++ src/io/generics/MyCustomList.java
@@ -1,6 +1,18 @@
 package io.generics;
 
+import java.util.ArrayList;
+
 public class MyCustomList {
 
+	ArrayList<String> list = new ArrayList<>();
+	
+	public void addElement(String element){
+		list.add(element);
+	}
+	
+	public void removeElement(String element){
+		list.remove(element);
+	}
+	
 }
 
