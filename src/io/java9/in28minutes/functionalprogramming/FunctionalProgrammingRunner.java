5d858405524fe098ae04246347b4c3d137bf3ef9
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-15 11:43:44 +0530

    Added list

--- src/io/java9/in28minutes/functionalprogramming/FunctionalProgrammingRunner.java
+++ src/io/java9/in28minutes/functionalprogramming/FunctionalProgrammingRunner.java
@@ -1,3 +1,10 @@
 package io.java9.in28minutes.functionalprogramming;
 
-public class FunctionalProgrammingRunner {}
+import java.util.List;
+
+public class FunctionalProgrammingRunner {
+	
+	public static void main(String[] args) {
+		List<String> list = List.of("Appple", "Orange", "Banana", "Cat", "Dog");
+	}
+}
