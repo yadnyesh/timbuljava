4792c2c7353aa9e20905901054d512688550a128
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-15 11:45:08 +0530

    First Functional Programming

--- src/io/java9/in28minutes/functionalprogramming/FunctionalProgrammingRunner.java
+++ src/io/java9/in28minutes/functionalprogramming/FunctionalProgrammingRunner.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class FunctionalProgrammingRunner {
 	
+	private static void printWithFP(List<String> list){
+		list.stream().forEach(element -> System.out.println(element));
+	}
+	
 	public static void main(String[] args) {
 		List<String> list = List.of("Appple", "Orange", "Banana", "Cat", "Dog");
 	}
