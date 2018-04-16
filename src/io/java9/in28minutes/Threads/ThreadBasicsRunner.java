c15cdaca8c11556a902a84a4f3a8fa3fa317a6e6
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-16 12:18:59 +0530

    Task1 as one Thread

--- src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
+++ src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
@@ -1,12 +1,19 @@
 package io.java9.in28minutes.Threads;
 
-public class ThreadBasicsRunner {
-	
-	public static void main(String[] args) {
+
+class Task1 extends Thread {
+	public void run() {
 		for (int i = 101; i <=199; i++) {
 			System.out.print(i + " " );
 		}
 		System.out.println("\nTask 1 Done! ");
+	}
+}
+
+public class ThreadBasicsRunner {
+	
+	public static void main(String[] args) {
+		
 		for (int i = 201; i <=299; i++) {
 			System.out.print(i + " " );
 		}
