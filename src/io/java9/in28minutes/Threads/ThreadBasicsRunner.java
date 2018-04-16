3ade1a7fbdac4777606e67a968b79e7e0306d62d
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-16 12:20:30 +0530

    Run Task1 as one Thread

--- src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
+++ src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
@@ -14,13 +14,16 @@ public class ThreadBasicsRunner {
 	
 	public static void main(String[] args) {
 		
+		Task1 task1 = new Task1();
+		task1.run();
+		
 		for (int i = 201; i <=299; i++) {
 			System.out.print(i + " " );
 		}
-		System.out.println("\nTask 1 Done! ");
+		System.out.println("\nTask 2 Done! ");
 		for (int i = 301; i <=399; i++) {
 			System.out.print(i + " " );
 		}
-		System.out.println("\nTask 1 Done! ");
+		System.out.println("\nTask 3 Done! ");
 	}
 }
