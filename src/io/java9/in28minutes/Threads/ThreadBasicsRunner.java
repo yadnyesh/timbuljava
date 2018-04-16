0af48f0d2fd9902f4e4c40c5c65bb85c30c3360a
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-16 12:15:59 +0530

    Refactor

--- src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
+++ src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
@@ -6,14 +6,14 @@ public class ThreadBasicsRunner {
 		for (int i = 101; i <=199; i++) {
 			System.out.print(i + " " );
 		}
-		System.out.println("\n Task 1 Done! ");
+		System.out.println("\nTask 1 Done! ");
 		for (int i = 201; i <=299; i++) {
 			System.out.print(i + " " );
 		}
-		System.out.println("\n Task 1 Done! ");
+		System.out.println("\nTask 1 Done! ");
 		for (int i = 301; i <=399; i++) {
 			System.out.print(i + " " );
 		}
-		System.out.println("\n Task 1 Done! ");
+		System.out.println("\nTask 1 Done! ");
 	}
 }
