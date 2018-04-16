469c1d8d5cfe0f96b39f074e3760a3df5027ab3b
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-16 13:34:10 +0530

    Start another thread - Runnable interface

--- src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
+++ src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
@@ -11,20 +11,31 @@ class Task1 extends Thread {
 	}
 }
 
-public class ThreadBasicsRunner {
+class Task2 implements Runnable {
 	
-	public static void main(String[] args) {
-		
-		Task1 task1 = new Task1();
-		task1.start();
-		
+	@Override
+	public void run() {
 		System.out.println("Task 2 started");
 		for (int i = 201; i <=299; i++) {
 			System.out.print(i + " " );
 		}
 		System.out.println("\nTask 2 Done! ");
+	}
+}
+
+public class ThreadBasicsRunner {
+	
+	public static void main(String[] args) {
 		
-		System.out.println("Task 3 started");
+		System.out.println("Task1 kicked off");
+		Task1 task1 = new Task1();
+		task1.start();
+		
+		System.out.println("Task2 kicked off");
+		Task2 task2 = new Task2();
+		Thread task2Thread = new Thread(task2);
+		
+		System.out.println("Task 3 kicked off");
 		for (int i = 301; i <=399; i++) {
 			System.out.print(i + " " );
 		}
