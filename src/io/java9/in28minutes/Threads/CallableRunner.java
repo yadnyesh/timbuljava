731ba6a571718d12505d544807c79735a1c58850
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-16 15:36:00 +0530

    CallableTask with Callable

--- src/io/java9/in28minutes/Threads/CallableRunner.java
+++ src/io/java9/in28minutes/Threads/CallableRunner.java
@@ -1,6 +1,6 @@
 package io.java9.in28minutes.Threads;
 
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
 class CallableTask implements Callable<String>{
 	
@@ -20,7 +20,12 @@ class CallableTask implements Callable<String>{
 
 public class CallableRunner {
 	
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+		Future<String> in28minutes = executorService.submit(new CallableTask("in28minutes"));
+		System.out.println("\n CallableTask(\"in28minutes\") executed");
+		String welcomeMessage = in28minutes.get();
+		System.out.println(welcomeMessage);
+		System.out.println("\n Main Completed");
 	}
 }
