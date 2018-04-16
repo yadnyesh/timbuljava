c047cfd1693d812d7ae526fd3dec46aef8f8b6e7
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-17 01:05:31 +0530

    Introducing to TRY-CATCH

---
+++ src/io/java9/in28minutes/exceptionHandling/ExceptionHandlingRunner2.java
@@ -0,0 +1,24 @@
+package io.java9.in28minutes.exceptionHandling;
+
+public class ExceptionHandlingRunner2 {
+	
+	public static void main(String[] args) {
+		method1();
+		System.out.println("Main Ended");
+	}
+	
+	private static void method1() {
+		method2();
+		System.out.println("Method 1 completed");
+	}
+	
+	private static void method2() {
+		try {
+			String str = null;
+			str.length();
+			System.out.println("Method 2 completed");
+		} catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+}
