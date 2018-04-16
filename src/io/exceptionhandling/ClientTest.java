1295e762171f714f36707774d6ac4e303804ef3e
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-16 21:20:41 +0530

    Completed ClientTest

--- src/io/exceptionhandling/ClientTest.java
+++ src/io/exceptionhandling/ClientTest.java
@@ -1,5 +1,25 @@
 package io.exceptionhandling;
 
-public class ClientTest {
+import java.util.Scanner;
 
+public class ClientTest {
+	
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)){
+			System.out.println("Enter person age: ");
+			int age = scanner.nextInt();
+			new ClientTest().calculateExtraBenefits(age);
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private void calculateExtraBenefits(int age) {
+		if (age < 60){
+			System.out.println("Benefits not applicable");
+			throw InvalidAgeException(age);
+		} else {
+			System.out.println("Benefits Extended!");
+		}
+	}
 }
