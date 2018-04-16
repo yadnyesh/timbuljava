e08f142a53d394f9c4fa5febea15de25ff601846
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-17 01:02:39 +0530

    Exception flow.

--- src/io/java9/in28minutes/exceptionHandling/ExceptionHandlingRunner.java
+++ src/io/java9/in28minutes/exceptionHandling/ExceptionHandlingRunner.java
@@ -1,5 +1,19 @@
 package io.java9.in28minutes.exceptionHandling;
 
 public class ExceptionHandlingRunner {
-
+	
+	public static void main(String[] args) {
+		method1();
+		System.out.println("Main Ended");
+	}
+	
+	private static void method1() {
+		method2();
+		System.out.println("Method 2 completed");
+	}
+	
+	private static void method2() {
+		String str = null;
+		str.length();
+	}
 }
