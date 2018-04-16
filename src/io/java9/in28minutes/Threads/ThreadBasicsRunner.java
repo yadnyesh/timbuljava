a9b5b5740e779d4b5ffe2eca5989a8b79c1b93b0
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-16 12:15:37 +0530

    Code before multi-threading

--- src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
+++ src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
@@ -1,5 +1,19 @@
 package io.java9.in28minutes.Threads;
 
 public class ThreadBasicsRunner {
-
+	
+	public static void main(String[] args) {
+		for (int i = 101; i <=199; i++) {
+			System.out.print(i + " " );
+		}
+		System.out.println("\n Task 1 Done! ");
+		for (int i = 201; i <=299; i++) {
+			System.out.print(i + " " );
+		}
+		System.out.println("\n Task 1 Done! ");
+		for (int i = 301; i <=399; i++) {
+			System.out.print(i + " " );
+		}
+		System.out.println("\n Task 1 Done! ");
+	}
 }
