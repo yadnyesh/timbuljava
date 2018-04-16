cceaa448adb6befb8e433bba4dccd93c4351db5a
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-16 13:31:33 +0530

    Start another thread - Refactor

--- src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
+++ src/io/java9/in28minutes/Threads/ThreadBasicsRunner.java
@@ -18,10 +18,13 @@ public class ThreadBasicsRunner {
 		Task1 task1 = new Task1();
 		task1.start();
 		
+		System.out.println("Task 2 started");
 		for (int i = 201; i <=299; i++) {
 			System.out.print(i + " " );
 		}
 		System.out.println("\nTask 2 Done! ");
+		
+		System.out.println("Task 3 started");
 		for (int i = 301; i <=399; i++) {
 			System.out.print(i + " " );
 		}
