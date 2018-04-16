1348cb5588c93596eec02f0b84d32dca97306c00
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-16 15:53:45 +0530

    MultipleAnyCallableRunner

--- src/io/java9/in28minutes/Threads/MultipleAnyCallableRunner.java
+++ src/io/java9/in28minutes/Threads/MultipleAnyCallableRunner.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class MultipleCallableRunner {
+public class MultipleAnyCallableRunner {
 	
 	public static void main(String[] args) throws InterruptedException {
 		ExecutorService executorService = Executors.newFixedThreadPool(3);
