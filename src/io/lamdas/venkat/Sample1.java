a6a19061fa598b838dcf26613cff3786a5ff6adb
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-23 14:07:26 +0530

    Starting with Venkat's code

--- src/io/lamdas/venkat/Sample1.java
+++ src/io/lamdas/venkat/Sample1.java
@@ -1,5 +1,11 @@
 package io.lamdas.venkat;
 
-public class Sample1 {
+import java.util.Arrays;
+import java.util.List;
 
+public class Sample1 {
+	
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6);
+	}
 }
