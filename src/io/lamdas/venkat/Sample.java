0be7eae905c60d15a858f9213d1d3ad67d325f0d
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-30 12:37:25 +0530

    Refactored code

--- src/io/lamdas/venkat/Sample.java
+++ src/io/lamdas/venkat/Sample.java
@@ -3,13 +3,7 @@ package io.lamdas.venkat;
 public class Sample {
 	
 	public static void main(String[] args) {
-		Thread th = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Thread t = Thread.currentThread();
-				System.out.println("Test: " + t.getName());
-			}
-		});
+		Thread th = new Thread(() -> System.out.println("Test: " + Thread.currentThread().getName()));
 		th.start();
 		Thread t = Thread.currentThread();
 		System.out.println("Test1: " + t.getName());
