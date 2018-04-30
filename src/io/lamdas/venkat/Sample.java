91c0a3d879808c1c3ade0f9500311b793f4f7b6e
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-04-30 12:32:58 +0530

    Print Thread names

--- src/io/lamdas/venkat/Sample.java
+++ src/io/lamdas/venkat/Sample.java
@@ -1,3 +1,18 @@
 package io.lamdas.venkat;
 
-public class Sample {}
+public class Sample {
+	
+	public static void main(String[] args) {
+		Thread th = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				Thread t = Thread.currentThread();
+				System.out.println("Test: " + t.getName());
+			}
+		});
+		th.start();
+		Thread t = Thread.currentThread();
+		System.out.println("Test1: " + t.getName());
+		
+	}
+}
