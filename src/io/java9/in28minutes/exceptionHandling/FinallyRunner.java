64f41fb90217d7c14d8e2ca6f8dedf2f285c30ae
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-05-03 15:30:02 +0530

    Update Exception Handling with Finally block

--- src/io/java9/in28minutes/exceptionHandling/FinallyRunner.java
+++ src/io/java9/in28minutes/exceptionHandling/FinallyRunner.java
@@ -1,18 +1,27 @@
 package io.java9.in28minutes.exceptionHandling;
 
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class FinallyRunner {
 	
 	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
 		try {
-			Scanner scanner = new Scanner(System.in);
+			
 			int[] numbers = {1,2,3,4,5};
 			int number = numbers[6];
-			scanner.close();
+			
 		} catch(Exception e){
 			e.printStackTrace();
+		} finally {
+			System.out.println("Just before closing scanner");
+			scanner.close();
 		}
+		
+		System.out.println("This line should be printed");
 
 	}
 }
