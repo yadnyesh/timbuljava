5de99cd23a9a2f112fdb5539053e946a1dab784f
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-05-03 15:24:28 +0530

    Code without Exception Handling

--- src/io/java9/in28minutes/exceptionHandling/FinallyRunner.java
+++ src/io/java9/in28minutes/exceptionHandling/FinallyRunner.java
@@ -1,3 +1,13 @@
 package io.java9.in28minutes.exceptionHandling;
 
-public class FinallyRunner {}
+import java.util.Scanner;
+
+public class FinallyRunner {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] numbers = {1,2,3,4,5};
+		int number = numbers[6];
+		scanner.close();
+	}
+}
