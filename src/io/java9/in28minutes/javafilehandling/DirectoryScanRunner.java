649f9804aeec04c0243488b81418d9cd46b3a2d0
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-05-05 19:49:11 +0530

    List all the directories

--- src/io/java9/in28minutes/javafilehandling/DirectoryScanRunner.java
+++ src/io/java9/in28minutes/javafilehandling/DirectoryScanRunner.java
@@ -1,5 +1,15 @@
 package io.java9.in28minutes.javafilehandling;
 
-public class DirectoryScanRunner {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class DirectoryScanRunner {
+	
+	public static void main(String[] args) throws IOException {
+		Files.list(Paths.get("/Users/z063407/tuts/targetcode/JavaCollections/"))
+			 .forEach(System.out::println);
+		;
+	}
 }
