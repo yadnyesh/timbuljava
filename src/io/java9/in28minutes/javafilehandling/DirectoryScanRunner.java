8d71d97bf929d55826048d1b7e8e6f1a0a79de1f
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-05-05 19:54:59 +0530

    Recursively walk through the files

--- src/io/java9/in28minutes/javafilehandling/DirectoryScanRunner.java
+++ src/io/java9/in28minutes/javafilehandling/DirectoryScanRunner.java
@@ -1,6 +1,7 @@
 package io.java9.in28minutes.javafilehandling;
 
 import java.io.IOException;
+import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,8 +9,10 @@ import java.nio.file.Paths;
 public class DirectoryScanRunner {
 	
 	public static void main(String[] args) throws IOException {
-		Files.list(Paths.get("/Users/z063407/tuts/targetcode/JavaCollections/"))
+		String currentDirectory = "/Users/z063407/tuts/targetcode/JavaCollections/";
+		Files.list(Paths.get(currentDirectory))
 			 .forEach(System.out::println);
-		;
+		Files.walk(Paths.get(currentDirectory), 1)
+				.forEach(System.out::println);
 	}
 }
