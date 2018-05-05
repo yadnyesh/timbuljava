1489e80a706606d22313bce70d1441e84d03bc7f
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-05-05 09:37:45 +0530

    code for adding amounts

--- src/io/java9/in28minutes/exceptionHandling/Amount.java
+++ src/io/java9/in28minutes/exceptionHandling/Amount.java
@@ -1,5 +1,31 @@
 package io.java9.in28minutes.exceptionHandling;
 
 public class Amount {
+	private String currency;
+	private int amount;
+	
+	public Amount(String currency, int amount) {
+		this.currency = currency;
+		this.amount = amount;
+	}
+	
+	public void add(Amount amount){
+		this.amount += amount.amount;
+	}
+	
+	@Override
+	public String toString() {
+		return "Amount{" + "currency='" + currency + '\'' + ", amount=" + amount + '}';
+	}
+	
+	
+}
 
+class ThrowingExceptionRunner {
+	
+	public static void main(String[] args) {
+		Amount amount1 = new Amount("USD", 10);
+		Amount amount2 = new Amount("USD", 20);
+		amount1.add(amount2);
+	}
 }
