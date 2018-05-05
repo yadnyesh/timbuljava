8b3de3c4933ca57c4cee335521475bea557d9218
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-05-05 23:54:16 +0530

    ConcurrentHashMap

--- src/io/java9/in28minutes/collections/ConcurrentMapRunner.java
+++ src/io/java9/in28minutes/collections/ConcurrentMapRunner.java
@@ -1,5 +1,18 @@
 package io.java9.in28minutes.collections;
 
-public class ConcurrentMapRunner {
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.LongAdder;
 
+public class ConcurrentMapRunner {
+	public static void main(String[] args) {
+		String str = "ABCD ABCD ABCD";
+		ConcurrentMap<Character, LongAdder> occurances = new ConcurrentHashMap<>();
+		for (char character: str.toCharArray()){
+			occurances.computeIfAbsent(character, ch -> new LongAdder()).increment();
+		}
+		System.out.println(occurances);
+	}
 }
