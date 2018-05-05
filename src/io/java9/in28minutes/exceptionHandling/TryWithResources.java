3cd537044201dd0c426397a5ad64e9f17f6693ba
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-05-05 13:14:53 +0530

    Example - TryWithResources

--- src/io/java9/in28minutes/exceptionHandling/TryWithResources.java
+++ src/io/java9/in28minutes/exceptionHandling/TryWithResources.java
@@ -5,13 +5,11 @@ import java.util.Scanner;
 public class TryWithResources {
 	
 	public static void main(String[] args) {
-		
-		Scanner scanner = null;
-			scanner = new Scanner(System.in);
-			int[] numbers = {1,2,3,4,5};
-			int number = numbers[6];
+			try (Scanner scanner = new Scanner(System.in)){
+				int[] numbers = {1, 2, 3, 4, 5};
+				int number = numbers[6];
 				scanner.close();
-		System.out.println("This line should be printed");
-
+				System.out.println("This line should be printed");
+		}
 	}
 }
