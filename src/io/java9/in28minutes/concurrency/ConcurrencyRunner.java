7158c9af8bb531b71bb87a173bdf53863e29e758
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-05-05 22:44:49 +0530

    Created ConcurrencyRunner method

---
+++ src/io/java9/in28minutes/concurrency/ConcurrencyRunner.java
@@ -0,0 +1,12 @@
+package io.java9.in28minutes.concurrency;
+
+public class ConcurrencyRunner {
+	
+	public static void main(String[] args) {
+		Counter counter = new Counter();
+		counter.increment();
+		counter.increment();
+		counter.increment();
+		System.out.println(counter.getI());
+	}
+}
