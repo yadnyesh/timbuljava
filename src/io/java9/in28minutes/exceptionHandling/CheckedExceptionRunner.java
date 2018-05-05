69f55d73396e3409200aa666cd7c16d81385492a
Yadnyesh <yadnyesh.bharatjuvekar@target.com>
2018-05-05 09:24:54 +0530

    Catch Exception or use Throw

--- src/io/java9/in28minutes/exceptionHandling/CheckedExceptionRunner.java
+++ src/io/java9/in28minutes/exceptionHandling/CheckedExceptionRunner.java
@@ -2,8 +2,17 @@ package io.java9.in28minutes.exceptionHandling;
 
 public class CheckedExceptionRunner {
 	
+	public static void main(String[] args) {
+		try {
+			someOtherMethod();
+			Thread.sleep(2000);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	public CheckedExceptionRunner() {
+	private static void someOtherMethod() throws InterruptedException {
+		Thread.sleep(2000);
 	}
 	
 }
