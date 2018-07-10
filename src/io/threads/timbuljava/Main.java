97729aa28c157be4d2d2dbb491aa225987f2f43c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-10 20:13:19 +0530

    Predicates in Java - https://www.youtube.com/watch?v=Eq8C_X5M1Lw - Trying Predicate for the first time.

--- src/io/threads/timbuljava/Main.java
+++ src/io/threads/timbuljava/Main.java
@@ -4,5 +4,10 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello from Main Thread!");
+		
+		Thread anotherThread = new AnotherThread();
+		anotherThread.start();
+		
+		System.out.println("Again from the Main Thread");
 	}
 }
