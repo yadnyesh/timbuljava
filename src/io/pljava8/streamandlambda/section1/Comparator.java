4fec3d0ebb41139a5ace7371b80e910bfab45a85
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-27 20:18:02 +0530

    Using Function interface for the first time.

--- src/io/pljava8/streamandlambda/section1/Comparator.java
+++ src/io/pljava8/streamandlambda/section1/Comparator.java
@@ -3,4 +3,5 @@ package io.pljava8.streamandlambda.section1;
 @FunctionalInterface
 public interface Comparator<T> {
 	public int compare(T t1, T t2);
+	public static
 }
--- src/io/pljava8/streamandlambda/section1/MainComparator.java
+++ src/io/pljava8/streamandlambda/section1/MainComparator.java
@@ -1,11 +1,15 @@
 package io.pljava8.streamandlambda.section1;
 
+import java.util.function.Function;
+
 public class MainComparator {
 	
 	public static void main(String[] args) {
 		Comparator<Person> cmpAge = (p1, p2) -> p2.getAge() - p1.getAge();
 		Comparator<Person> cmpFirstName = (p1, p2) -> p2.getFirstName().compareTo(p1.getFirstName());
 		Comparator<Person> cmpLastName = (p1, p2) -> p2.getLastName().compareTo(p1.getLastName());
+		
+		Function<Person, Integer> f1 = p -> p.getAge();
 		
 	}
 }
