e3f208e9c862bae507b306aad5d89527df4599e0
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-27 14:38:11 +0530

    Comparator for lastName

--- src/io/pljava8/streamandlambda/section1/MainComparator.java
+++ src/io/pljava8/streamandlambda/section1/MainComparator.java
@@ -5,5 +5,7 @@ public class MainComparator {
 	public static void main(String[] args) {
 		Comparator<Person> cmpAge = (p1, p2) -> p2.getAge() - p1.getAge();
 		Comparator<Person> cmpFirstName = (p1, p2) -> p2.getFirstName().compareTo(p1.getFirstName());
+		Comparator<Person> cmpLastName = (p1, p2) -> p2.getLastName().compareTo(p1.getLastName());
+		
 	}
 }
