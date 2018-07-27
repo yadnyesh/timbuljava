282fc417504b009cecd8913f160dc1b0a71f6efe
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-27 14:32:43 +0530

    Defined - Person class

--- src/io/pljava8/streamandlambda/Person.java
+++ src/io/pljava8/streamandlambda/Person.java
@@ -1,3 +1,41 @@
 package io.pljava8.streamandlambda;
 
-public class Person {}
+public class Person {
+	String firstName;
+	String lastName;
+	int age;
+	
+	public Person() {
+	}
+	
+	public Person(String firstName, String lastName, int age) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
