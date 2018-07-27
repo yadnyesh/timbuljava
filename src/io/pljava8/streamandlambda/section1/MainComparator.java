34e02f8769b2a94bd527aca8668b20debe7d5eb5
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-27 14:36:39 +0530

    Comparator for Age

--- src/io/pljava8/streamandlambda/section1/MainComparator.java
+++ src/io/pljava8/streamandlambda/section1/MainComparator.java
@@ -3,6 +3,6 @@ package io.pljava8.streamandlambda.section1;
 public class MainComparator {
 	
 	public static void main(String[] args) {
-	
+		Comparator<Person> cmpAge = (p1, p2) -> p2.getAge() - p1.getAge();
 	}
 }
