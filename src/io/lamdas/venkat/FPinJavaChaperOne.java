123e1617b943783cffc9103cd83b33ab4cdabbdc
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-29 11:01:22 +0530

    Write a lambda expression

--- src/io/lamdas/venkat/FPinJavaChaperOne.java
+++ src/io/lamdas/venkat/FPinJavaChaperOne.java
@@ -9,4 +9,9 @@ public class FPinJavaChaperOne {
 	final List<BigDecimal> prices = Arrays.asList(new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
 												new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
 												new BigDecimal("45"), new BigDecimal("17"));
+	
+	final BigDecimal totalOfDiscountedPrices = prices.stream()
+			.filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
+			.map(price -> price.multiply(BigDecimal.valueOf(0.9)))
+			.reduce(BigDecimal.ZERO, BigDecimal::add);
 }
