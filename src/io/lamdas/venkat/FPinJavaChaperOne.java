4b0550a32e4ed3583f3d48c4b378b70fb76e77ed
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-29 20:23:12 +0530

    New For loop to print collections

--- src/io/lamdas/venkat/FPinJavaChaperOne.java
+++ src/io/lamdas/venkat/FPinJavaChaperOne.java
@@ -8,6 +8,8 @@ public class FPinJavaChaperOne {
 	
 	public static void main(String[] args) {
 		final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
-		
+		for (String name: friends){
+			System.out.println(name);
+		}
 	}
 }
