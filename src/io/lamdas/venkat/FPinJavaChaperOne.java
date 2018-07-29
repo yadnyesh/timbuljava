b51780dbce5473b8f1405a2d1578b9ba0f40f366
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-29 11:06:03 +0530

    Write a lambda expression

--- src/io/lamdas/venkat/FPinJavaChaperOne.java
+++ src/io/lamdas/venkat/FPinJavaChaperOne.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FPinJavaChaperOne {
-
-	final List<BigDecimal> prices = Arrays.asList(new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
-												new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
-												new BigDecimal("45"), new BigDecimal("17"));
 	
-	final BigDecimal totalOfDiscountedPrices = prices.stream()
-			.filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
-			.map(price -> price.multiply(BigDecimal.valueOf(0.9)))
-			.reduce(BigDecimal.ZERO, BigDecimal::add);
+	public static void main(String[] args) {
+		
+		final List<BigDecimal> prices = Arrays.asList(new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
+				new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
+				new BigDecimal("45"), new BigDecimal("17"));
+		
+		final BigDecimal totalOfDiscountedPrices = prices.stream()
+				.filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
+				.map(price -> price.multiply(BigDecimal.valueOf(0.9)))
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+		
+		System.out.printf("Total of Discounted prices is: " + totalOfDiscountedPrices);
+	}
 }
