a70ec45671f37a7f22a4c7d6c325e050bd5bd512
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-29 20:25:58 +0530

    passing an anonymous function

--- src/io/lamdas/venkat/FPinJavaChaperOne.java
+++ src/io/lamdas/venkat/FPinJavaChaperOne.java
@@ -3,13 +3,17 @@ package io.lamdas.venkat;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class FPinJavaChaperOne {
 	
 	public static void main(String[] args) {
 		final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
-		for (String name: friends){
-			System.out.println(name);
-		}
+		friends.forEach(new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				System.out.println(s);
+			}
+		});
 	}
 }
