cef64de6084569185f93cddb98eba177545d3adc
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-29 20:22:30 +0530

    Create a collection of Strings

--- src/io/lamdas/venkat/FPinJavaChaperOne.java
+++ src/io/lamdas/venkat/FPinJavaChaperOne.java
@@ -7,16 +7,7 @@ import java.util.List;
 public class FPinJavaChaperOne {
 	
 	public static void main(String[] args) {
+		final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
 		
-		final List<BigDecimal> prices = Arrays.asList(new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
-				new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
-				new BigDecimal("45"), new BigDecimal("17"));
-		
-		final BigDecimal totalOfDiscountedPrices = prices.stream()
-				.filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
-				.map(price -> price.multiply(BigDecimal.valueOf(0.9)))
-				.reduce(BigDecimal.ZERO, BigDecimal::add);
-		
-		System.out.printf("Total of Discounted prices is: " + totalOfDiscountedPrices);
 	}
 }
