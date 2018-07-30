af5926c3acc960c35f607d6959f60631d62c9241
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-30 09:42:34 +0530

    Using method reference

--- src/io/lamdas/venkat/FPinJavaChaperOne.java
+++ src/io/lamdas/venkat/FPinJavaChaperOne.java
@@ -10,7 +10,7 @@ public class FPinJavaChaperOne {
 	public static void main(String[] args) {
 		final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
 		friends.forEach(System.out::println);
-		friends.stream().map(name -> name.toUpperCase())
+		friends.stream().map(String::toUpperCase)
 		.forEach(System.out::println);
 		friends.stream().map(name -> name.length())
 		.forEach(length -> System.out.println("Length of the string is: " + length));
