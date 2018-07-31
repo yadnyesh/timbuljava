3ba10aca0bc751874bcba69d630643e9c9869998
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-31 10:30:01 +0530

    Format Text - replace "." with newline character

--- src/io/adapterpattern/NewLineFormatter.java
+++ src/io/adapterpattern/NewLineFormatter.java
@@ -4,7 +4,8 @@ public class NewLineFormatter implements TextFormattable{
 	
 	@Override
 	public String formatText(String text) {
-		return null;
+		String formattedText = text.replace(".", "\n");
+		return formattedText;
 	}
 	
 }
