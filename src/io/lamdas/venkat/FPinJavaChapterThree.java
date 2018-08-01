d5897a222695571343af3f785e4d7cbf1739d385
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-01 19:26:47 +0530

    feat: Starting with a new chapter

--- src/io/lamdas/venkat/FPinJavaChapterThree.java
+++ src/io/lamdas/venkat/FPinJavaChapterThree.java
@@ -1,5 +1,10 @@
 package io.lamdas.venkat;
 
 public class FPinJavaChapterThree {
-
+	
+	
+	public static void main(String[] args) {
+		final String str = "w00t";
+		str.chars().forEach(c -> System.out.println(c));
+	}
 }
