47819e16e6b256be797a25178ebc20b530c78cd2
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-01 10:00:58 +0530

    feat: Printing Java SQL date

--- src/io/yadnyesh/javanotesforprofessional/UtilToSqlConversion.java
+++ src/io/yadnyesh/javanotesforprofessional/UtilToSqlConversion.java
@@ -5,5 +5,12 @@ public class UtilToSqlConversion {
 	public static void main(String[] args) {
 		java.util.Date utilDate = new java.util.Date();
 		System.out.println("java.util.Date is : " + utilDate);
+		java.sql.Date sqlDate = convert(utilDate);
+		System.out.println("java.sql.Date is : " + utilDate);
+	}
+	
+	private static java.sql.Date convert (java.util.Date utilDate) {
+		java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+		return sqlDate;
 	}
 }
