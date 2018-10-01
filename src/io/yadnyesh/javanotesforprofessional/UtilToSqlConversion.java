a2d72a301399ecb7d18fbcdafc450307f6de5bc7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-01 09:58:06 +0530

    feat: Printing Java Util date

---
+++ src/io/yadnyesh/javanotesforprofessional/UtilToSqlConversion.java
@@ -0,0 +1,9 @@
+package io.yadnyesh.javanotesforprofessional;
+
+public class UtilToSqlConversion {
+	
+	public static void main(String[] args) {
+		java.util.Date utilDate = new java.util.Date();
+		System.out.println("java.util.Date is : " + utilDate);
+	}
+}
