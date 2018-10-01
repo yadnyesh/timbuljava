7a5a638776e96a3766b8a18963abdd78d92d8741
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-01 10:03:42 +0530

    feat: Format java.util.Date

--- src/io/yadnyesh/javanotesforprofessional/UtilToSqlConversion.java
+++ src/io/yadnyesh/javanotesforprofessional/UtilToSqlConversion.java
@@ -1,5 +1,8 @@
 package io.yadnyesh.javanotesforprofessional;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class UtilToSqlConversion {
 	
 	public static void main(String[] args) {
@@ -7,6 +10,8 @@ public class UtilToSqlConversion {
 		System.out.println("java.util.Date is : " + utilDate);
 		java.sql.Date sqlDate = convert(utilDate);
 		System.out.println("java.sql.Date is : " + utilDate);
+		DateFormat df = new SimpleDateFormat("dd/MM/YYYY - hh:mm:ss");
+		System.out.println("Formatted date is "  + df.format(utilDate));
 	}
 	
 	private static java.sql.Date convert (java.util.Date utilDate) {
