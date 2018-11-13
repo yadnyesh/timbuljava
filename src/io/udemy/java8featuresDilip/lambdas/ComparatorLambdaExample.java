e6ce8c137d164b710361ebc0cac5c825215c8cab
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-13 19:22:18 +0530

    feat: Implement comparator interface

---
+++ src/io/udemy/java8featuresDilip/lambdas/ComparatorLambdaExample.java
@@ -0,0 +1,19 @@
+package io.udemy.java8featuresDilip.lambdas;
+
+import java.util.Comparator;
+
+public class ComparatorLambdaExample {
+	
+	public static void main(String[] args) {
+		/**
+		 * prior java 8
+		 */
+		
+		Comparator<Integer> comparator = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2);
+			}
+		};
+	}
+}
