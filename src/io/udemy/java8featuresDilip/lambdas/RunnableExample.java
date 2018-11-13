253bfaa2c6e893045b79af79cbe83ea311834cfc
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-13 11:42:33 +0530

    feat: Runnable with Lambda and non-lambda

---
+++ src/io/udemy/java8featuresDilip/lambdas/RunnableExample.java
@@ -0,0 +1,28 @@
+package io.udemy.java8featuresDilip.lambdas;
+
+public class RunnableExample {
+	
+	public static void main(String[] args) {
+		/*
+			prior to Java 8
+		 */
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Inside Runnable 1");
+			}
+		};
+		
+		new Thread(runnable).start();
+		
+		/*
+			using lambdas
+		 */
+		
+		Runnable runnablelambda = () -> {
+			System.out.println("Inside Runnable with lambda");
+		};
+		
+		new Thread(runnablelambda).start();
+	}
+}
