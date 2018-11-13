599844c2601e6f578d84519fab625cd061b75b06
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-13 10:59:24 +0530

    feat: Imperative style of Programming

---
+++ src/io/udemy/java8featuresDilip/ImpvsDec.java
@@ -0,0 +1,20 @@
+package io.udemy.java8featuresDilip;
+
+public class ImpvsDec {
+	
+	public static void main(String[] args) {
+	
+//		Imperative - how
+		
+		int sum = 0;
+		
+		for (int i= 0; i <= 100; i++) {
+			sum += i;
+		}
+		System.out.println("Sum using Imperative style: " + sum);
+		
+		
+//		Declarative - What
+	
+	}
+}
