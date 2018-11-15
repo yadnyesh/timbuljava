6485cc3bf72fa1073c2913e605d87f98e8a6d814
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-15 19:21:53 +0530

    feat: Functiona Interface example 1: Section 6 - Lecture 13

--- src/io/udemy/java8featuresDilip/functionalinterfaces/ConsumerExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/ConsumerExample.java
@@ -1,8 +1,13 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
+import java.util.function.Consumer;
+
 public class ConsumerExample {
 	
 	public static void main(String[] args) {
-	
+		Consumer<String> consumerString = (s) -> System.out.println(s.toUpperCase());
+		
+		consumerString.accept("yadnyesh");
+		
 	}
 }
