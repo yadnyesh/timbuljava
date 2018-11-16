a552f12656edf810f59bc36783978c6a5e0c8ba7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-16 19:35:20 +0530

    feat: created PredicateAndConsumerExample

--- src/io/udemy/java8featuresDilip/functionalinterfaces/PredicateAndConsumerExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/PredicateAndConsumerExample.java
@@ -1,3 +1,39 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
-public class PredicateAndConsumerExample {}
+import io.udemy.java8featuresDilip.data.Student;
+import io.udemy.java8featuresDilip.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class PredicateAndConsumerExample {
+	
+	Predicate<Student> studentPredicateGrade = ((student) -> {
+		return student.getGradeLevel() >= 3;
+	});
+	
+	Predicate<Student> studentPredicateGpa = ((student) -> {
+		return student.getGpa() >= 3.9;
+	});
+	
+	BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name + " : " + activities);
+	
+	Consumer<Student> studentConsumer = ((student) -> {
+		if(studentPredicateGrade.and(studentPredicateGpa).test(student)){
+			studentBiConsumer.accept(student.getName(), student.getActivities());
+		}
+	});
+	
+	public void printNameAndActivities(List<Student> students) {
+		students.forEach(studentConsumer);
+	}
+	
+	public static void main(String[] args) {
+		
+		List<Student> studentList = StudentDataBase.getAllStudents();
+		new PredicateAndConsumerExample().printNameAndActivities(studentList);
+		
+	}
+}
