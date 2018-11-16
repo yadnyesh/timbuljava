71a09c241805d269ede8fcf27203f8514735b3e6
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-16 13:11:27 +0530

    feat: starting with Predicates - working

--- src/io/udemy/java8featuresDilip/functionalinterfaces/PredicateExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/PredicateExample.java
@@ -1,5 +1,12 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
+import java.util.function.Predicate;
+
 public class PredicateExample {
+	
+	public static void main(String[] args) {
+		Predicate<Integer> p = (i) -> {return i%2 == 0;};
+		System.out.println(p.test(4));
+	}
 
 }
