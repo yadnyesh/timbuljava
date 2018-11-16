6a35f83164dff1d8d18cd2e8c027617681020109
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-16 10:17:23 +0530

    feat: Functional Interface Example

--- src/io/udemy/java8featuresDilip/functionalinterfaces/ConsumerExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/ConsumerExample.java
@@ -1,12 +1,26 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
+
+import io.udemy.java8featuresDilip.data.Student;
+import io.udemy.java8featuresDilip.data.StudentDataBase;
+import io.yadnyesh.StudentMain;
+
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
 	
+	static void printName() {
+		Consumer<Student> c2 = (student) -> System.out.println(student);
+		List<Student> studentList = StudentDataBase.getAllStudents();
+		studentList.forEach(c2);
+	}
+	
+	
 	public static void main(String[] args) {
 		Consumer<String> consumerString = (s) -> System.out.println(s.toUpperCase());
 		consumerString.accept("yadnyesh");
+		printName();
 		
 	}
 }
