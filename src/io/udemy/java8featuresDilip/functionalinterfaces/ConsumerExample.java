b9d4f83ee1703bb28be879ce7d565b57bb38e008
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-16 10:29:13 +0530

    feat: Functional Interface - with conditions

--- src/io/udemy/java8featuresDilip/functionalinterfaces/ConsumerExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/ConsumerExample.java
@@ -3,32 +3,39 @@ package io.udemy.java8featuresDilip.functionalinterfaces;
 
 import io.udemy.java8featuresDilip.data.Student;
 import io.udemy.java8featuresDilip.data.StudentDataBase;
-import io.yadnyesh.StudentMain;
 
 import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
 	
+	static Consumer<Student> consumerStudent = (student) -> System.out.println(student);
+	static Consumer<Student> consumerStudentName = (student) -> System.out.print(student.getName());
+	static Consumer<Student> consumerStudentActivities = (student) -> System.out.println(student.getActivities());
+	static List<Student> studentList = StudentDataBase.getAllStudents();
+	
 	public static void printName() {
-		Consumer<Student> c2 = (student) -> System.out.println(student);
-		List<Student> studentList = StudentDataBase.getAllStudents();
-		studentList.forEach(c2);
+		studentList.forEach(consumerStudent);
 	}
 	
 	public static void printNameAndActivities() {
-		Consumer<Student> consumerStudentName = (student) -> System.out.print(student.getName());
-		Consumer<Student> consumerStudentActivities = (student) -> System.out.println(student.getActivities());
-		List<Student> studentList = StudentDataBase.getAllStudents();
 		studentList.forEach(consumerStudentName.andThen(consumerStudentActivities));
 	}
 	
+	public static void printNameAndActivitiesUsingCondition() {
+		studentList.forEach(student -> {
+			if(student.getGradeLevel() >= 3) {
+				consumerStudentName.andThen(consumerStudentActivities);
+			}
+		});
+	}
 	
 	public static void main(String[] args) {
 		Consumer<String> consumerString = (s) -> System.out.println(s.toUpperCase());
 		consumerString.accept("yadnyesh");
 		printName();
 		printNameAndActivities();
+		printNameAndActivitiesUsingCondition();
 		
 	}
 }
