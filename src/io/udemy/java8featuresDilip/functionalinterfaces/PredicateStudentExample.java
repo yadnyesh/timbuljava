ecb34fb57493e8029651327d4aedaf290450d168
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-16 18:51:55 +0530

    feat: in-line lambda that uses Predicate

--- src/io/udemy/java8featuresDilip/functionalinterfaces/PredicateStudentExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/PredicateStudentExample.java
@@ -1,3 +1,25 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
-public class PredicateStudentExample {}
+import io.udemy.java8featuresDilip.data.Student;
+import io.udemy.java8featuresDilip.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateStudentExample {
+	
+	static Predicate<Student> studentPredicate = (s) -> s.getGradeLevel() >= 3;
+	
+	public static void filterStudentByGradeLevel() {
+		List<Student> studentList = StudentDataBase.getAllStudents();
+		studentList.forEach((student -> {
+			if(studentPredicate.test(student)){
+				System.out.println(student);
+			}
+		}));
+	}
+	
+	public static void main(String[] args) {
+		filterStudentByGradeLevel();
+	}
+}
