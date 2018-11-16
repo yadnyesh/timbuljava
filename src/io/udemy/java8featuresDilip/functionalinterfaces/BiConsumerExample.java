f31a8ea04c10f1a37944eb847df4e9c6a263db46
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-16 12:56:14 +0530

    feat: Functional Interface - created - BiConsumerExample

--- src/io/udemy/java8featuresDilip/functionalinterfaces/BiConsumerExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/BiConsumerExample.java
@@ -1,15 +1,19 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
 import io.udemy.java8featuresDilip.data.Student;
+import io.udemy.java8featuresDilip.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
 
 public class BiConsumerExample {
 	
+	static List<Student> studentList = StudentDataBase.getAllStudents();
+	
 	public static void main(String[] args) {
-		BiConsumer<String, String> biConsumer = (a,b) -> System.out.println("a: " + a + " b: " + b);
-		biConsumer.accept("yadnyesh", "bharatjuvekar");
+		
+		BiConsumer<String, List<String>> biConsumerStudentWithActivities = (name, activities) -> System.out.println(name + " : " + activities);
+		studentList.forEach(student -> biConsumerStudentWithActivities.accept(student.getName(), student.getActivities()));
 	}
 	
 }
