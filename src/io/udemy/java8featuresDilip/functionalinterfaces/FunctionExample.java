f5fb18812e16e40c8d3124cc02216dc353de74f3
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-17 00:35:00 +0530

    feat: created FunctionExample

--- src/io/udemy/java8featuresDilip/functionalinterfaces/FunctionExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/FunctionExample.java
@@ -1,3 +1,14 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
-public class FunctionExample {}
+import java.util.function.Function;
+
+public class FunctionExample {
+	
+	static Function<String, String> firstFunction = (name) -> name.toUpperCase();
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Result is : " + firstFunction.apply("yadnyesh"));
+	
+	}
+}
