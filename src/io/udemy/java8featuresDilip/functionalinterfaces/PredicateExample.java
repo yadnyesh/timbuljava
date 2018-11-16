79d6b516a81359259e0f8da043f7693b8a3d3cdd
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-16 13:11:57 +0530

    feat: starting with Predicates - code refactoring

--- src/io/udemy/java8featuresDilip/functionalinterfaces/PredicateExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/PredicateExample.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 public class PredicateExample {
 	
 	public static void main(String[] args) {
-		Predicate<Integer> p = (i) -> {return i%2 == 0;};
+		Predicate<Integer> p = (i) -> i%2 == 0;
 		System.out.println(p.test(4));
 	}
 
