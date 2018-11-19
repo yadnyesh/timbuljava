35eedf3c92276329a7a2c8350a6bb3fcca3f12a8
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-19 19:28:34 +0530

    feat: complete return single student

--- src/io/udemy/java8featuresDilip/functionalinterfaces/SupplierExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/SupplierExample.java
@@ -1,8 +1,10 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
 import io.udemy.java8featuresDilip.data.Student;
+import io.udemy.java8featuresDilip.data.StudentDataBase;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class SupplierExample {
@@ -12,7 +14,11 @@ public class SupplierExample {
 			return new Student("Adam",2,3.6, "male", Arrays.asList("swimming", "basketball","volleyball"));
 		};
 		
+		Supplier<List<Student>> listSupplier = () -> StudentDataBase.getAllStudents();
+		
 		System.out.println("Student is: " + studentSupplier.get());
+		
+		System.out.println("Students are: " + listSupplier.get());
 	}
 
 }
