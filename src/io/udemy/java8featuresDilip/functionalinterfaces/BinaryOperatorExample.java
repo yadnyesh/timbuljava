d2044ec10bd17c750c551ec7478d129c027f3afe
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-19 18:22:33 +0530

    feat: completed BinaryOperatorExample

--- src/io/udemy/java8featuresDilip/functionalinterfaces/BinaryOperatorExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/BinaryOperatorExample.java
@@ -1,5 +1,11 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
-public class BinaryOperatorExample {
+import java.util.function.BinaryOperator;
 
+public class BinaryOperatorExample {
+	
+	public static void main(String[] args) {
+		BinaryOperator<Integer> binaryOperator = (a,b) -> a*b;
+		System.out.println(binaryOperator.apply(3,5));
+	}
 }
