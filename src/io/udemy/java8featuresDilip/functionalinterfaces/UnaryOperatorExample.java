5dc8f3ea4047a68c8f9fa31c63a98eb6f1baf015
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-19 18:14:49 +0530

    feat: complete UnaryOperatorExample

--- src/io/udemy/java8featuresDilip/functionalinterfaces/UnaryOperatorExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/UnaryOperatorExample.java
@@ -1,9 +1,13 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
+import java.util.function.UnaryOperator;
+
 public class UnaryOperatorExample {
 	
-	public static void main(String[] args) {
+	static UnaryOperator<String> stringUnaryOperator = (s) -> s.concat(" Default String");
 	
+	public static void main(String[] args) {
+		System.out.printf(stringUnaryOperator.apply("java 8"));
 	}
 
 }
