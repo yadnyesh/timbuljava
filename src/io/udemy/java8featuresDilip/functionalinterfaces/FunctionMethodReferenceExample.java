2aa2caad3fca710b0099a30e61794fbacd68641d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-19 19:59:55 +0530

    feat: completed with MethodReference

--- src/io/udemy/java8featuresDilip/functionalinterfaces/FunctionMethodReferenceExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/FunctionMethodReferenceExample.java
@@ -1,5 +1,18 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
+import java.util.function.Function;
+
 public class FunctionMethodReferenceExample {
+	
+	static Function<String, String> toUpperCaseLambda = (s) -> s.toUpperCase();
+	
+	static Function<String, String> toUpperCaseMethodReference = String::toUpperCase;
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Using Lambda: " + toUpperCaseLambda.apply("Yadnyesh"));
+		System.out.println("Using Method Reference: " + toUpperCaseLambda.apply("Yadnyesh"));
+		
+	}
 
 }
