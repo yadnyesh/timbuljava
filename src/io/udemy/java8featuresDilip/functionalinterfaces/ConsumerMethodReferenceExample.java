d90e05741f4554a723c37827ef5f2c5c74320995
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-19 20:04:05 +0530

    feat: created ConsumerMethodReferenceExample - before lamdba

--- src/io/udemy/java8featuresDilip/functionalinterfaces/ConsumerMethodReferenceExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/ConsumerMethodReferenceExample.java
@@ -1,5 +1,23 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
+import io.udemy.java8featuresDilip.data.Student;
+import io.udemy.java8featuresDilip.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.Consumer;
+
 public class ConsumerMethodReferenceExample {
+	
+	
+	static Consumer<Student> consumerStudent = (student) -> System.out.println(student);
+	static Consumer<Student> consumerStudentName = (student) -> System.out.print(student.getName());
+	static Consumer<Student> consumerStudentActivities = (student) -> System.out.println(student.getActivities());
+	static List<Student> studentList = StudentDataBase.getAllStudents();
+	
+	public static void main(String[] args) {
+		
+		StudentDataBase.getAllStudents().forEach(consumerStudent);
+		
+	}
 
 }
