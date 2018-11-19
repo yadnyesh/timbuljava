02bc2d4c91a9db49cfd7b745fa7a134c91cc88d6
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-19 19:15:10 +0530

    feat: completed MaxBy using Comparator

--- src/io/udemy/java8featuresDilip/functionalinterfaces/BinaryOperatorExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/BinaryOperatorExample.java
@@ -1,11 +1,16 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
+import java.util.Comparator;
 import java.util.function.BinaryOperator;
 
 public class BinaryOperatorExample {
 	
+	static Comparator<Integer> comparator = (a,b) -> a.compareTo(b);
+	
 	public static void main(String[] args) {
 		BinaryOperator<Integer> binaryOperator = (a,b) -> a*b;
 		System.out.println(binaryOperator.apply(3,5));
+		BinaryOperator<Integer> maxBy = BinaryOperator.maxBy(comparator);
+		System.out.println("Result of MaxBy is: " + maxBy.apply(4,5));
 	}
 }
