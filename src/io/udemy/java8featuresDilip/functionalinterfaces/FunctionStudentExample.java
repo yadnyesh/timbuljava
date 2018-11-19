89d4cedcf116921ef832cb462f78a87c141c629b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-19 17:58:09 +0530

    feat: created BiFunctionalExample

--- src/io/udemy/java8featuresDilip/functionalinterfaces/FunctionStudentExample.java
+++ src/io/udemy/java8featuresDilip/functionalinterfaces/FunctionStudentExample.java
@@ -1,5 +1,24 @@
 package io.udemy.java8featuresDilip.functionalinterfaces;
 
-public class FunctionStudentExample {
+import io.udemy.java8featuresDilip.data.Student;
+import io.udemy.java8featuresDilip.data.StudentDataBase;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class FunctionStudentExample {
+	
+	public static Function<List<Student>, Map<String, Double>> studentFunction = (students -> {
+		Map<String, Double> studentGradeMap = new HashMap<>();
+		students.forEach(student -> {
+			studentGradeMap.put(student.getName(), student.getGpa());
+		});
+		return studentGradeMap;
+	});
+	
+	public static void main(String[] args) {
+		System.out.println(studentFunction.apply(StudentDataBase.getAllStudents()));
+	}
 }
