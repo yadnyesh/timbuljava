4c6f8f7cebd923b50cd0146c596c39c2d3c23e63
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-01-14 15:54:48 +0530

    feat: Section 2, Lecture 9: SAM interface + Invoke Lambda --> return String length

--- src/io/lamdas/udemy/durga/InterfaceSAM.java
+++ src/io/lamdas/udemy/durga/InterfaceSAM.java
@@ -1,7 +1,9 @@
 package io.lamdas.udemy.durga;
 
+@FunctionalInterface
 public interface InterfaceSAM {
 	
-	public void addTwoNumbers(int firstNum, int secondNum);
+	//public void addTwoNumbers(int firstNum, int secondNum);
+	public int getStringLength(String s);
 	
 }
--- src/io/lamdas/udemy/durga/InvokeLambda.java
+++ src/io/lamdas/udemy/durga/InvokeLambda.java
@@ -4,7 +4,9 @@ public class InvokeLambda {
 	
 	public static void main(String[] args) {
 		
-		InterfaceSAM interfaceSAM = (a,b) -> System.out.println("The sum of the two numbers is: " + (a+b));
-		interfaceSAM.addTwoNumbers(20, 30);
+//		InterfaceSAM interfaceSAM = (a,b) -> System.out.println("The sum of the two numbers is: " + (a+b));
+//		interfaceSAM.addTwoNumbers(20, 30);
+		InterfaceSAM interfaceSAM = s -> s.length();
+		System.out.printf("Lambda implementation ---> " + interfaceSAM.getStringLength("Yadnyesh"));
 	}
 }
