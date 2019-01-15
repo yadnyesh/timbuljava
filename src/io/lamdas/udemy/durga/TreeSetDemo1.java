7c0bc5514ee54d32ed0ab8c53899f894268deb6d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-01-15 22:18:28 +0530

    feat: Section 3, Lecture 17: Implemented Comparator with lambda expression - beautiful - TreeSet

---
+++ src/io/lamdas/udemy/durga/TreeSetDemo1.java
@@ -0,0 +1,20 @@
+package io.lamdas.udemy.durga;
+
+import java.util.Collections;
+import java.util.TreeSet;
+
+public class TreeSetDemo1 {
+	
+	
+	public static void main(String[] args) {
+		TreeSet<Integer> treeSet = new TreeSet<Integer>((i1, i2) -> (i1 > i2) ? -1 : (i1 < i2) ? 1: 0);
+		treeSet.add(10);
+		treeSet.add(11);
+		treeSet.add(9);
+		treeSet.add(1);
+		treeSet.add(2);
+		treeSet.add(5);
+		System.out.println("Natural sorting: " + treeSet);
+		
+	}
+}
