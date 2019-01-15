f038b8fc7f9baec1736c7e3a1c1b5c2abdb69c20
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-01-15 21:54:30 +0530

    feat: Section 3, Lecture 15: Implemented Comparator

---
+++ src/io/lamdas/udemy/durga/ComparatorDemo.java
@@ -0,0 +1,34 @@
+package io.lamdas.udemy.durga;
+
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class ComparatorDemo {
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		arrayList.add(10);
+		arrayList.add(90);
+		arrayList.add(60);
+		arrayList.add(5);
+		arrayList.add(0);
+		arrayList.add(2);
+		Collections.sort(arrayList, new MyComparator());
+		System.out.println(arrayList);
+	}
+	
+	
+
+}
+
+class MyComparator implements Comparator<Integer> {
+	
+	@Override
+	public int compare(Integer t1, Integer t2) {
+		return (t1 > t2) ? -1: (t1 < t2) ? 1 : 0 ;
+	}
+}
