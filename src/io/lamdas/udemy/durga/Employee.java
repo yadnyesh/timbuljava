6b1555a8d4602ff0327b3b1e8b2f96c72a6d9c44
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-01-21 05:46:58 -0600

    feat: Section 3, Lecture 19: Created Employee class

---
+++ src/io/lamdas/udemy/durga/Employee.java
@@ -0,0 +1,11 @@
+package io.lamdas.udemy.durga;
+
+public class Employee {
+	int employeeNo;
+	int employeeName;
+	
+	public Employee(int employeeNo, int employeeName) {
+		this.employeeNo = employeeNo;
+		this.employeeName = employeeName;
+	}
+}
