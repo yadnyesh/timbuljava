c2cd1dc402fcce6e7d08683ef1f7ba24771bf5dd
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-01-21 05:50:39 -0600

    feat: Section 3, Lecture 19: Print Employee object

--- src/io/lamdas/udemy/durga/Employee.java
+++ src/io/lamdas/udemy/durga/Employee.java
@@ -2,10 +2,18 @@ package io.lamdas.udemy.durga;
 
 public class Employee {
 	int employeeNo;
-	int employeeName;
+	String employeeName;
 	
-	public Employee(int employeeNo, int employeeName) {
+	public Employee() {
+	}
+	
+	public Employee(int employeeNo, String employeeName) {
 		this.employeeNo = employeeNo;
 		this.employeeName = employeeName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee{" + "employeeNo=" + employeeNo + ", employeeName=" + employeeName + '}';
 	}
 }
---
+++ src/io/lamdas/udemy/durga/EmployeeLambda.java
@@ -0,0 +1,10 @@
+package io.lamdas.udemy.durga;
+
+public class EmployeeLambda {
+	
+	public static void main(String[] args) {
+		Employee e1 = new Employee(100, "Durga");
+		System.out.println(e1);
+	}
+	
+}
