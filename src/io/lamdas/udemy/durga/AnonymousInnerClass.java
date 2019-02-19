0dd524fd109ad5551cc28f9550780f587aeed377
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-02-19 17:46:12 +0530

    feat: Section 4, Lecture 20: Anonymous Inner Class without Lambda

--- src/io/lamdas/udemy/durga/AnonymousInnerClass.java
+++ src/io/lamdas/udemy/durga/AnonymousInnerClass.java
@@ -1,20 +1,12 @@
 package io.lamdas.udemy.durga;
 
 public class AnonymousInnerClass {
-
-	
 	public static void main(String[] args) {
-		Runnable r = new Runnable() {
-			@Override
-			public void run() {
-				for (int i = 0; i < 10; i++) {
-					System.out.println("Child Thread");
-				}
+		Thread t = new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Child Thread");
 			}
-		};
-		Thread t = new Thread(r);
+		});
 		t.start();
 	}
-	
-	
 }
