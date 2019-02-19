a37572d68f912859b4f9a46a1aad1e4c52004fdf
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-02-19 18:06:25 +0530

    feat: Section 4, Lecture 20: Anonymous Inner Class

--- src/io/lamdas/udemy/durga/AnonymousInnerClass.java
+++ src/io/lamdas/udemy/durga/AnonymousInnerClass.java
@@ -1,12 +1,36 @@
 package io.lamdas.udemy.durga;
 
 public class AnonymousInnerClass {
-	public static void main(String[] args) {
-		Thread t = new Thread(() -> {
-			for (int i = 0; i < 10; i++) {
-				System.out.println("Child Thread");
+	
+	int x = 888;
+	
+	
+	interface MyInterface {
+		void printX();
+	}
+	
+	public void myPrintClass()
+	{
+		MyInterface myInterface = new MyInterface() {
+			int x = 999;
+			@Override
+			public void printX() {
+				System.out.println(this.x);
 			}
-		});
-		t.start();
+		};
+		myInterface.printX();
+	}
+	
+	
+	public static void main(String[] args) {
+//		Thread t = new Thread(() -> {
+//			for (int i = 0; i < 10; i++) {
+//				System.out.println("Child Thread");
+//			}
+//		});
+//		t.start();
+		AnonymousInnerClass anonymousInnerClass = new AnonymousInnerClass();
+		anonymousInnerClass.myPrintClass();
+	
 	}
 }
