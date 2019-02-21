a093724a52b7ebfbb76a2de1f0abf63b45065309
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-02-21 13:25:32 +0530

    feat: Completable future, Youtube - https://www.youtube.com/watch?v=ImtZgX1nmr8

---
+++ src/io/completetablefuture/CompletetableFutureExample.java
@@ -0,0 +1,34 @@
+package io.completetablefuture;
+
+import javafx.concurrent.Task;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CompletetableFutureExample {
+	
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		
+		Future<Integer> future = executorService.submit(new Task());
+		
+		try {
+			Integer resultInteger = future.get();
+			System.out.println("Result from the task is: " + resultInteger);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	static class Task implements Callable<Integer> {
+		
+		@Override
+		public Integer call() {
+			return new Random().nextInt();
+		}
+	}
+}
