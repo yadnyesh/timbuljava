01551513d43fec4007b090a6e367f0e17cde4307
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-02-21 09:02:29 +0530

    feat: Section 5, Lecture 24: Interface with Default methods

--- src/io/lamdas/udemy/durga/ImplementInterfaceWithDefaultMethods.java
+++ src/io/lamdas/udemy/durga/ImplementInterfaceWithDefaultMethods.java
@@ -2,6 +2,10 @@ package io.lamdas.udemy.durga;
 
 public class ImplementInterfaceWithDefaultMethods implements DefaultMethodInInterface{
 	
+	public void printMessage(String message) {
+		System.out.println("The encoded message is: " + message);
+	}
+	
 	public static void main(String[] args) {
 		ImplementInterfaceWithDefaultMethods implementInterfaceWithDefaultMethods = new ImplementInterfaceWithDefaultMethods();
 		implementInterfaceWithDefaultMethods.printMessage("India vs Aus on 27th Feb");
