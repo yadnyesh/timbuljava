f31bbf923ae25c79d68296de0add1a295f8cbd60
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-02-26 10:27:00 +0530

    feat: Hands-on JODA time, https://www.youtube.com/watch?v=lYiSm9cWkAU

--- src/io/lamdas/udemy/durga/dateandtime/LocalDateExample.java
+++ src/io/lamdas/udemy/durga/dateandtime/LocalDateExample.java
@@ -1,6 +1,7 @@
 package io.lamdas.udemy.durga.dateandtime;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class LocalDateExample {
@@ -11,6 +12,11 @@ public class LocalDateExample {
 		System.out.println(localDate);
 		LocalTime localTime = LocalTime.now();
 		System.out.println(localTime);
+		LocalDateTime localDateTime = LocalDateTime.now();
+		System.out.println(localDateTime);
+		LocalDateTime myLocalDateTime = LocalDateTime.of(1996, 12, 23, 19, 30, 00);
+		System.out.println(myLocalDateTime);
+		System.out.println("After 10 months -> " + localDate.plusMonths(10));
 	}
 	
 }
