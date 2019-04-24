f8f472b5e73dd55757461b85865da3ee9c46d693
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-04-24 14:45:03 +0530

    feat: Section 14, Lecture 208

--- src/io/udemy/timbuljava/Hello.java
+++ src/io/udemy/timbuljava/Hello.java
@@ -3,6 +3,25 @@ package io.udemy.timbuljava;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World " + divideEAFP(0,0));
+		
 	}
+	
+	private static int divideLBYL(int x, int y) {
+		if(y != 0){
+			return x / y;
+		} else {
+			return 0;
+		}
+	}
+	
+	private static int divideEAFP(int x, int y) {
+		try {
+			return x /y;
+		} catch(Exception e){
+			System.out.println(e.toString());
+			return 0;
+		}
+	}
+	
 }
