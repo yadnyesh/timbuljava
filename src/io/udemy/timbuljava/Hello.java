c9aaa95fb7c8f9ac719b8d163badeec038b7e3cb
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-05-04 22:10:01 +0530

    For daily commit

--- src/io/udemy/timbuljava/Hello.java
+++ src/io/udemy/timbuljava/Hello.java
@@ -3,7 +3,7 @@ package io.udemy.timbuljava;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World " + divideEAFP(0,0));
+		System.out.println("Hello World for commit " + divideEAFP(0,0));
 		
 	}
 	
