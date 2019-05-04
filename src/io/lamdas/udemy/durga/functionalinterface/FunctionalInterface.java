23dd32b5145b2963d8664a3b5d695f7ffc4c8943
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-05-04 23:03:51 +0530

    feat: Section 7, Lecture 39 - Functional Interface

--- src/io/lamdas/udemy/durga/functionalinterface/FunctionalInterface.java
+++ src/io/lamdas/udemy/durga/functionalinterface/FunctionalInterface.java
@@ -7,5 +7,8 @@ public class FunctionalInterface {
 	public static void main(String[] args) {
 		Function<String, Integer> stringIntegerFunction = s -> s.length();
 		System.out.println("Using Functional Predicate: " + stringIntegerFunction.apply("yadnyesh"));
+		
+		Function<Integer, Integer> returnSquareFunction = i -> i * i;
+		System.out.println("Square of the number " + 5 + " is: " + returnSquareFunction.apply(5));
 	}
 }
